75851ba89c9e61e8549b59e296b33a29bf58aff0
bonustrack <fabien@bonustrack.co>
2020-09-13 17:37:19 +0700

    Add strategy erc20-balance-of-fixed-total

---
+++ src/strategies/erc20-balance-of-fixed-total/index.ts
@@ -0,0 +1,12 @@
+import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
+
+export async function strategy(provider, addresses, options, snapshot) {
+  const score = await erc20BalanceOfStrategy(provider, addresses, options, snapshot);
+  const totalScore = Object.values(score).reduce((a, b) => a + b, 0);
+  return Object.fromEntries(
+    Object.entries(score).map(address => [
+      address[0],
+      options.total * address[1] / totalScore
+    ])
+  );
+}
--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,9 +1,11 @@
 import { strategy as balancer } from './balancer';
 import { strategy as erc20BalanceOf } from './erc20-balance-of';
+import { strategy as erc20BalanceOfFixedTotal } from './erc20-balance-of-fixed-total';
 import { strategy as makerDsChief } from './maker-ds-chief';
 
 export default {
   balancer,
   erc20BalanceOf,
+  erc20BalanceOfFixedTotal,
   makerDsChief
 };
