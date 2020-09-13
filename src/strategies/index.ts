198a8458ceee75c3ffba0c17ba2c760a42d9357e
bonustrack <fabien@bonustrack.co>
2020-09-13 23:34:33 +0700

    Add strategy erc20-balance-of-cv

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,11 +1,13 @@
 import { strategy as balancer } from './balancer';
 import { strategy as erc20BalanceOf } from './erc20-balance-of';
 import { strategy as erc20BalanceOfFixedTotal } from './erc20-balance-of-fixed-total';
+import { strategy as erc20BalanceOfCv } from './erc20-balance-of-cv';
 import { strategy as makerDsChief } from './maker-ds-chief';
 
 export default {
   balancer,
   erc20BalanceOf,
   erc20BalanceOfFixedTotal,
+  erc20BalanceOfCv,
   makerDsChief
 };
