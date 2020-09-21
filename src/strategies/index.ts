1c3ced1a5eace77969996d7773cb8990c342a0ea
bonustrack <fabien@bonustrack.co>
2020-09-22 00:47:12 +0700

    Add Uni strategy

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -3,11 +3,13 @@ import { strategy as erc20BalanceOf } from './erc20-balance-of';
 import { strategy as erc20BalanceOfFixedTotal } from './erc20-balance-of-fixed-total';
 import { strategy as erc20BalanceOfCv } from './erc20-balance-of-cv';
 import { strategy as makerDsChief } from './maker-ds-chief';
+import { strategy as uni } from './uni';
 
 export default {
   balancer,
   'erc20-balance-of': erc20BalanceOf,
   'erc20-balance-of-fixed-total': erc20BalanceOfFixedTotal,
   'erc20-balance-of-cv': erc20BalanceOfCv,
-  'maker-ds-chief': makerDsChief
+  'maker-ds-chief': makerDsChief,
+  'uni': uni
 };
---
+++ src/strategies/uni/index.ts
@@ -0,0 +1,28 @@
+import { formatUnits } from '@ethersproject/units';
+import { multicall } from '../../utils';
+import { abi } from './Uni.json';
+
+const UNI_ADDRESS = {
+  1: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
+}
+
+export async function strategy(network, provider, addresses, options, snapshot) {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+  const response = await multicall(
+    network,
+    provider,
+    abi,
+    addresses.map((address: any) => [
+      UNI_ADDRESS[network],
+      'getPriorVotes',
+      [address, snapshot]
+    ]),
+    { blockTag }
+  );
+  return Object.fromEntries(
+    response.map((value, i) => [
+      addresses[i],
+      parseFloat(formatUnits(value.toString(), options.decimals))
+    ])
+  );
+}
