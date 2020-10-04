c1689c8c42387cf6093d847b515cb7c5d7124fe2
bonustrack <fabien@bonustrack.co>
2020-10-04 23:13:00 +0700

    Use direct ABI item

--- src/strategies/erc20-balance-of/index.ts
+++ src/strategies/erc20-balance-of/index.ts
@@ -1,9 +1,32 @@
 import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
-import { abi } from './TestToken.json';
 
 export const author = 'bonustrack';
 export const version = '0.1.0';
+
+const abi = [
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'account',
+        type: 'address'
+      }
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  }
+];
 
 export async function strategy(network, provider, addresses, options, snapshot) {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
