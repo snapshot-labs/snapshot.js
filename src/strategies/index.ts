7681d1cc8369bdb9e9f42adc824e86dd173be143
Fabien <bonustrack@users.noreply.github.com>
2020-10-05 00:37:47 +0700

    Add Yearn vault strategy (#8)
    * Add author and version
    
    * Add ETH balance strategy
    
    * Use direct ABI item
    
    * Add Yearn vault strategy

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,17 +1,21 @@
 import { strategy as balancer } from './balancer';
+import { strategy as contractCall } from './contract-call';
 import { strategy as erc20BalanceOf } from './erc20-balance-of';
 import { strategy as erc20BalanceOfFixedTotal } from './erc20-balance-of-fixed-total';
 import { strategy as erc20BalanceOfCv } from './erc20-balance-of-cv';
+import { strategy as ethBalance } from './eth-balance';
 import { strategy as makerDsChief } from './maker-ds-chief';
 import { strategy as uni } from './uni';
-import { strategy as contractCall } from './contract-call';
+import { strategy as yearnVault } from './yearn-vault';
 
 export default {
   balancer,
+  'contract-call': contractCall,
   'erc20-balance-of': erc20BalanceOf,
   'erc20-balance-of-fixed-total': erc20BalanceOfFixedTotal,
   'erc20-balance-of-cv': erc20BalanceOfCv,
+  'eth-balance': ethBalance,
   'maker-ds-chief': makerDsChief,
-  'uni': uni,
-  'contract-call': contractCall
+  uni,
+  'yearn-vault': yearnVault
 };
--- src/strategies/uni/index.ts
+++ src/strategies/uni/index.ts
@@ -1,10 +1,36 @@
 import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
-import abi from './Uni.json';
+
+export const author = 'bonustrack';
+export const version = '0.1.0';
 
 const UNI_ADDRESS = {
   1: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
 }
+
+const abi = [
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'account',
+        type: 'address'
+      }
+    ],
+    name: 'getCurrentVotes',
+    outputs: [
+      {
+        internalType: 'uint96',
+        name: '',
+        type: 'uint96'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  }
+];
 
 export async function strategy(network, provider, addresses, options, snapshot) {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
