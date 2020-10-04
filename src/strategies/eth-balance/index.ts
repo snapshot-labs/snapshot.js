11d26c990f998784ff027b52e015288b8a76f463
bonustrack <fabien@bonustrack.co>
2020-10-04 23:11:29 +0700

    Add ETH balance strategy

---
+++ src/strategies/eth-balance/index.ts
@@ -0,0 +1,23 @@
+import { formatUnits } from '@ethersproject/units';
+import { multicall, MULTICALL } from '../../utils';
+import { abi } from '../../abi/Multicall.json';
+
+export const author = 'bonustrack';
+export const version = '0.1.0';
+
+export async function strategy(network, provider, addresses, options, snapshot) {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+  const response = await multicall(
+    network,
+    provider,
+    abi,
+    addresses.map((address: any) => [MULTICALL[network], 'getEthBalance', [address]]),
+    { blockTag }
+  );
+  return Object.fromEntries(
+    response.map((value, i) => [
+      addresses[i],
+      parseFloat(formatUnits(value.toString(), 18))
+    ])
+  );
+}
--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -2,6 +2,7 @@ import { strategy as balancer } from './balancer';
 import { strategy as erc20BalanceOf } from './erc20-balance-of';
 import { strategy as erc20BalanceOfFixedTotal } from './erc20-balance-of-fixed-total';
 import { strategy as erc20BalanceOfCv } from './erc20-balance-of-cv';
+import { strategy as ethBalance } from './eth-balance';
 import { strategy as makerDsChief } from './maker-ds-chief';
 import { strategy as uni } from './uni';
 import { strategy as contractCall } from './contract-call';
@@ -11,6 +12,7 @@ export default {
   'erc20-balance-of': erc20BalanceOf,
   'erc20-balance-of-fixed-total': erc20BalanceOfFixedTotal,
   'erc20-balance-of-cv': erc20BalanceOfCv,
+  'eth-balance': ethBalance,
   'maker-ds-chief': makerDsChief,
   'uni': uni,
   'contract-call': contractCall
