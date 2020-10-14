87b6bbac4d4623eaf57b8e3d58b23590180a80fa
pancake-swap <71247357+pancake-swap@users.noreply.github.com>
2020-10-14 16:51:02 +0800

    add pancake strategy (#10)
    * add pancake strategy
    
    * update author of pancake strategy
    
    * add readme for cake stratigy

---
+++ src/strategies/pancake/index.ts
@@ -0,0 +1,64 @@
+import { formatUnits } from "@ethersproject/units";
+import { multicall } from "../../utils";
+import { strategy as erc20BalanceOfStrategy } from "../erc20-balance-of";
+
+export const author = "pancake-swap";
+export const version = "0.0.1";
+
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "userInfo",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export async function strategy(network, provider, addresses, options, snapshot) {
+  const blockTag = typeof snapshot === "number" ? snapshot : "latest";
+  const score = await erc20BalanceOfStrategy(network, provider, addresses, options, snapshot);
+
+  const balances = await Promise.all(
+    options.chefAddresses.map((chefAddress) =>
+      multicall(
+        network,
+        provider,
+        abi,
+        addresses.map((address: any) => [
+          chefAddress,
+          "userInfo",
+          [address],
+          { blockTag },
+        ]),
+        { blockTag }
+      )
+    )
+  );
+
+  return Object.fromEntries(
+    Object.entries(score).map((address, index) => [
+      address[0],
+      address[1] +
+        balances.reduce(
+          (prev: number, cur: number) =>
+            prev + parseFloat(
+              formatUnits(cur[index].amount.toString(), options.decimals)
+            ),
+          0
+        ),
+    ])
+  );
+}
