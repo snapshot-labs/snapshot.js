f700a8a1cf54a9a7d875de2e2a0ea971babe0054
TSAN-HSIANG HSU <bun919tw@gmail.com>
2020-10-23 17:37:52 +0800

    src/strategies/: add cream-swap (#15)

---
+++ src/strategies/cream-swap/index.ts
@@ -0,0 +1,65 @@
+import { getAddress } from '@ethersproject/address';
+import { subgraphRequest } from '../../utils';
+
+export const author = 'bun919tw';
+export const version = '0.1.0';
+
+const CREAM_SWAP_SUBGRAPH_URL = {
+  1: 'https://api.thegraph.com/subgraphs/name/creamfinancedev/cream-swap-v2',
+  3: 'https://api.thegraph.com/subgraphs/name/creamfinancedev/cream-swap-dev'
+};
+
+export async function strategy(
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+) {
+  const params = {
+    poolShares: {
+      __args: {
+        where: {
+          userAddress_in: addresses.map((address) => address.toLowerCase()),
+          balance_gt: 0
+        },
+        first: 1000,
+        orderBy: 'balance',
+        orderDirection: 'desc'
+      },
+      userAddress: {
+        id: true
+      },
+      balance: true,
+      poolId: {
+        totalShares: true,
+        tokens: {
+          id: true,
+          balance: true
+        }
+      }
+    }
+  };
+  if (snapshot !== 'latest') {
+    // @ts-ignore
+    params.poolShares.__args.block = { number: snapshot };
+  }
+  const result = await subgraphRequest(CREAM_SWAP_SUBGRAPH_URL[network], params);
+  const score = {};
+  if (result && result.poolShares) {
+    result.poolShares.forEach((poolShare) =>
+      poolShare.poolId.tokens.map((poolToken) => {
+        const [, tokenAddress] = poolToken.id.split('-');
+        if (tokenAddress === options.address.toLowerCase()) {
+          const userAddress = getAddress(poolShare.userAddress.id);
+          if (!score[userAddress]) score[userAddress] = 0;
+          score[userAddress] =
+            score[userAddress] +
+            (poolToken.balance / poolShare.poolId.totalShares) *
+              poolShare.balance;
+        }
+      })
+    );
+  }
+  return score || {};
+}
--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,5 +1,6 @@
 import { strategy as balancer } from './balancer';
 import { strategy as contractCall } from './contract-call';
+import { strategy as creamSwap } from './cream-swap';
 import { strategy as erc20BalanceOf } from './erc20-balance-of';
 import { strategy as erc20BalanceOfCoeff } from './erc20-balance-of-coeff';
 import { strategy as erc20BalanceOfFixedTotal } from './erc20-balance-of-fixed-total';
@@ -15,6 +16,7 @@ import { strategy as synthetix } from './synthetix';
 export default {
   balancer,
   'contract-call': contractCall,
+  'cream-swap': creamSwap,
   'erc20-balance-of': erc20BalanceOf,
   'erc20-balance-of-fixed-total': erc20BalanceOfFixedTotal,
   'erc20-balance-of-cv': erc20BalanceOfCv,
