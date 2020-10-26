12441768e8b65290812f7aefe9d2de6c62993a28
BenjaminLu <coopermilk123@gmail.com>
2020-10-26 15:32:38 +0800

    Add Tokenlon strategy (#21)
    * Add TFT strategy
    
    * Format code
    
    * Rename strategy to erc20-with-balance

---
+++ src/strategies/erc20-with-balance/index.ts
@@ -0,0 +1,26 @@
+import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
+
+export const author = 'BenjaminLu';
+export const version = '0.1.0';
+
+export async function strategy(
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+) {
+  const score = await erc20BalanceOfStrategy(
+    network,
+    provider,
+    addresses,
+    options,
+    snapshot
+  );
+  return Object.fromEntries(
+    Object.entries(score).map((address: any) => [
+      address[0],
+      (address[1] > 0 ? 1 : 0)
+    ])
+  );
+}
