5f1e597e9aaabbde62faa8eed6a3d95cf9c73970
Benjamin Lu <coopermilk123@gmail.com>
2020-10-26 15:07:25 +0800

    Format code

--- src/strategies/tokenlon/index.ts
+++ src/strategies/tokenlon/index.ts
@@ -1,34 +1,7 @@
-import { formatUnits } from '@ethersproject/units';
-import { multicall } from '../../utils';
+import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
 
-export const author = 'TokenlonDev';
+export const author = 'BenjaminLu';
 export const version = '0.1.0';
-
-const TFT_ADDRESS = '0xE534619dEFDBF0cAf673b8AbF7158714F5BD4bd9';
-
-const abi = [
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'account',
-        type: 'address'
-      }
-    ],
-    name: 'balanceOf',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
-      }
-    ],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function'
-  }
-];
 
 export async function strategy(
   network,
@@ -37,18 +10,17 @@ export async function strategy(
   options,
   snapshot
 ) {
-  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
-  const response = await multicall(
+  const score = await erc20BalanceOfStrategy(
     network,
     provider,
-    abi,
-    addresses.map((address: any) => [TFT_ADDRESS, 'balanceOf', [address]]),
-    { blockTag }
+    addresses,
+    options,
+    snapshot
   );
   return Object.fromEntries(
-    response.map((value, i) => [
-      addresses[i],
-      (parseFloat(formatUnits(value.toString(), 0)) > 0 ? 1 : 0)
+    Object.entries(score).map((address: any) => [
+      address[0],
+      (address[1] > 0 ? 1 : 0)
     ])
   );
 }
