2366a7ade7f3a08ebd4a8ad8e18cb557580cf4c7
spaceforce <spaceforce-dev@gmail.com>
2020-11-05 13:55:40 +1100

    Update index.ts

--- src/strategies/erc20-balance-of/index.ts
+++ src/strategies/erc20-balance-of/index.ts
@@ -36,48 +36,17 @@ export async function strategy(
   snapshot
 ) {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
-
-  const increaceWeighting = (number, scalingFactor) => {
-    return number * scalingFactor
-  }
-
-  // Duplicate array - We can make the multicall in one call for both contracts on all addresses
-  addresses = [...addresses, ...addresses]
-
-  let omStakingResponse = [];
-
-  try {
-    omStakingResponse = await multicall(
-      network,
-      provider,
-      abi,
-      addresses.map((address: any, i: any) => [
-        (i < (addresses.length - addresses.length/2) - 1) ? options.omStakingAddress : options.omUniStakingAddress, 'balanceOf', [address]
-      ]),
-      { blockTag }
-    );
-
-    let om = (omStakingResponse.slice(0, addresses.length/2)).map((value, i) => [
+  const response = await multicall(
+    network,
+    provider,
+    abi,
+    addresses.map((address: any) => [options.address, 'balanceOf', [address]]),
+    { blockTag }
+  );
+  return Object.fromEntries(
+    response.map((value, i) => [
       addresses[i],
-      parseFloat(formatUnits((value).toString() , options.decimals))
+      parseFloat(formatUnits(value.toString(), options.decimals))
     ])
-  
-    let uni = (omStakingResponse.slice(addresses.length/2, addresses.length)).map((value, i) => [
-      addresses[i],
-      parseFloat(formatUnits((value).toString() , options.decimals))
-    ])
-
-    uni.forEach((uniBalance, i) => {
-      uni[i] = [uniBalance[0], (increaceWeighting(uni[i][1], options.omUniScalingFactor) + om[i][1])]
-    });
-    let combined = uni
-    return {scores: Object.fromEntries(
-      combined
-    )};
-    
-  } catch (error) {
-    console.log(error)
-  }
-  
-  
+  );
 }
