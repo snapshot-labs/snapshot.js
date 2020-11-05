ba0d2d032d2903bd753d23e650661466867e5328
bonustrack <fabien@bonustrack.co>
2020-11-05 21:32:47 +0700

    Update to latest suggestions

--- src/plugins/aragon/index.ts
+++ src/plugins/aragon/index.ts
@@ -2,39 +2,174 @@ import BN from 'bn.js';
 import { keccak256 } from '@ethersproject/keccak256';
 import { toUtf8Bytes } from '@ethersproject/strings';
 import { sendTransaction, subgraphRequest } from '../../utils';
-import abi from './GovernQueue.json';
 
 const ARAGON_SUBGRAPH_URL = {
-  4: 'https://api.thegraph.com/subgraphs/name/evalir/aragon-govern-rinkeby'
+  '1': 'https://thegraph.com/explorer/subgraph/aragon/aragon-govern-mainnet',
+  '4': 'https://thegraph.com/explorer/subgraph/aragon/aragon-govern-rinkeby'
 };
 
+const abi = [
+  {
+    inputs: [
+      {
+        components: [
+          {
+            components: [
+              {
+                internalType: 'uint256',
+                name: 'nonce',
+                type: 'uint256'
+              },
+              {
+                internalType: 'uint256',
+                name: 'executionTime',
+                type: 'uint256'
+              },
+              {
+                internalType: 'address',
+                name: 'submitter',
+                type: 'address'
+              },
+              {
+                internalType: 'contract IERC3000Executor',
+                name: 'executor',
+                type: 'address'
+              },
+              {
+                components: [
+                  {
+                    internalType: 'address',
+                    name: 'to',
+                    type: 'address'
+                  },
+                  {
+                    internalType: 'uint256',
+                    name: 'value',
+                    type: 'uint256'
+                  },
+                  {
+                    internalType: 'bytes',
+                    name: 'data',
+                    type: 'bytes'
+                  }
+                ],
+                internalType: 'struct ERC3000Data.Action[]',
+                name: 'actions',
+                type: 'tuple[]'
+              },
+              {
+                internalType: 'bytes32',
+                name: 'allowFailuresMap',
+                type: 'bytes32'
+              },
+              {
+                internalType: 'bytes',
+                name: 'proof',
+                type: 'bytes'
+              }
+            ],
+            internalType: 'struct ERC3000Data.Payload',
+            name: 'payload',
+            type: 'tuple'
+          },
+          {
+            components: [
+              {
+                internalType: 'uint256',
+                name: 'executionDelay',
+                type: 'uint256'
+              },
+              {
+                components: [
+                  {
+                    internalType: 'address',
+                    name: 'token',
+                    type: 'address'
+                  },
+                  {
+                    internalType: 'uint256',
+                    name: 'amount',
+                    type: 'uint256'
+                  }
+                ],
+                internalType: 'struct ERC3000Data.Collateral',
+                name: 'scheduleDeposit',
+                type: 'tuple'
+              },
+              {
+                components: [
+                  {
+                    internalType: 'address',
+                    name: 'token',
+                    type: 'address'
+                  },
+                  {
+                    internalType: 'uint256',
+                    name: 'amount',
+                    type: 'uint256'
+                  }
+                ],
+                internalType: 'struct ERC3000Data.Collateral',
+                name: 'challengeDeposit',
+                type: 'tuple'
+              },
+              {
+                internalType: 'address',
+                name: 'resolver',
+                type: 'address'
+              },
+              {
+                internalType: 'bytes',
+                name: 'rules',
+                type: 'bytes'
+              }
+            ],
+            internalType: 'struct ERC3000Data.Config',
+            name: 'config',
+            type: 'tuple'
+          }
+        ],
+        internalType: 'struct ERC3000Data.Container',
+        name: '_container',
+        type: 'tuple'
+      }
+    ],
+    name: 'schedule',
+    outputs: [
+      {
+        internalType: 'bytes32',
+        name: 'containerHash',
+        type: 'bytes32'
+      }
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function'
+  }
+];
+
 const GQL_QUERY = {
-  governQueue: {
+  registryEntry: {
     __args: {
-      id: '0xfc67dd2d7a67bc7b1aa5061c3f4b49f6c3341d00'
+      id: '$daoName'
     },
-    config: {
-      executionDelay: true,
-      scheduleDeposit: {
-        token: {
-          id: true
+    executor: {
+      address: true
+    },
+    queue: {
+      address: true,
+      config: {
+        executionDelay: true,
+        scheduleDeposit: {
+          token: true,
+          amount: true
         },
-        amount: true
-      },
-      challengeDeposit: {
-        token: {
-          id: true
+        challengeDeposit: {
+          token: true,
+          amount: true
         },
-        amount: true
-      },
-      vetoDeposit: {
-        token: {
-          id: true
-        },
-        amount: true
-      },
-      resolver: true,
-      rules: true
+        resolver: true,
+        rules: true
+      }
     }
   }
 };
@@ -51,13 +186,18 @@ const EMPTY_BYTES = '0x00';
  */
 async function scheduleAction(
   web3,
+  daoName,
   governQueueAddress,
   account,
   proof,
-  executor,
   actionsFromAragonPlugin
 ) {
-  const config = await subgraphRequest(ARAGON_SUBGRAPH_URL[4], GQL_QUERY);
+  const query = GQL_QUERY;
+  query.registryEntry.__args.id = daoName;
+  const { registryEntry } = await subgraphRequest(
+    ARAGON_SUBGRAPH_URL[4],
+    query
+  );
 
   // Building the nonce for the next tx
   const nonce = await web3.nonce();
@@ -66,40 +206,38 @@ async function scheduleAction(
   // We also need to get a timestamp bigger or equal to the current block.timestamp + config.executionDelay
   // Right now + execution delay + 60 seconds into the future
   const currentDate =
-    Math.round(Date.now() / 1000) + Number(config.executionDelay) + 60;
+    Math.round(Date.now() / 1000) +
+    Number(registryEntry.queue.config.executionDelay) +
+    60;
 
   return await sendTransaction(
     web3,
     abi,
     governQueueAddress,
-    'delayExecution',
+    'schedule',
     {
       payload: {
         nonce: newNonce.toString(),
         executionTime: currentDate,
         submitter: account,
-        executor,
+        executor: registryEntry.queue.executor.address,
         actions: actionsFromAragonPlugin,
         allowFailuresMap: FAILURE_MAP,
         // proof in snapshot's case, could be the proposal's IPFS CID
         proof: proof ? keccak256(toUtf8Bytes(proof)) : EMPTY_BYTES
       },
       config: {
-        executionDelay: config.executionDelay,
+        executionDelay: registryEntry.config.executionDelay,
         scheduleDeposit: {
-          token: config.scheduleDeposit.token.id,
-          amount: config.scheduleDeposit.amount
+          token: registryEntry.queue.config.scheduleDeposit.token.id,
+          amount: registryEntry.queue.config.scheduleDeposit.amount
         },
         challengeDeposit: {
-          token: config.challengeDeposit.token.id,
-          amount: config.challengeDeposit.amount
+          token: registryEntry.queue.config.challengeDeposit.token.id,
+          amount: registryEntry.queue.config.challengeDeposit.amount
         },
-        vetoDeposit: {
-          token: config.vetoDeposit.token.id,
-          amount: config.vetoDeposit.amount
-        },
-        resolver: config.resolver,
-        rules: config.rules
+        resolver: registryEntry.queue.config.resolver,
+        rules: registryEntry.queue.config.rules
       }
     },
     {
@@ -116,16 +254,21 @@ export default class Plugin {
   public website = 'https://aragon.org/blog/snapshot';
   public options: any;
 
-  async execute(web3, options, proposalOptions, id, winningChoice) {
+  async execute(
+    web3,
+    spaceOptions,
+    proposalOptions,
+    proposalId,
+    winningChoice
+  ) {
     try {
-      const account = '';
-      const executor = '';
+      const account = web3.getAddress();
       return await scheduleAction(
         web3,
-        options.governQueueAddress,
+        spaceOptions.daoName,
+        spaceOptions.governQueueAddress,
         account,
-        id,
-        executor,
+        proposalId,
         proposalOptions[`choice${winningChoice}`]
       );
     } catch (e) {
