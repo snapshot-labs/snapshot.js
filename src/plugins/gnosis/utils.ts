b17adce5855eee0f6df3a2909ea18de3b6b593c8
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-11-11 19:12:55 +0100

    Add Gnosis Omen plugin
    Create a new `gnosis` plugin with the Omen Subgraph to retrieve
    conditional tokens FixedProductMarketMaker contracts by a given
    `questionId`.

---
+++ src/plugins/gnosis/utils.ts
@@ -0,0 +1,50 @@
+import { subgraphRequest } from '../../utils';
+
+const OMEN_SUBGRAPH_URL = {
+  '1': 'https://api.thegraph.com/subgraphs/name/protofire/omen',
+  '4': 'https://api.thegraph.com/subgraphs/name/protofire/omen-rinkeby'
+};
+
+const GQL_QUERY = {
+  question: {
+    __args: {
+      id: undefined
+    },
+    id: true,
+    title: true,
+    conditions: {
+      id: true,
+      fixedProductMarketMakers: {
+        id: true,
+        collateralToken: true,
+        outcomeTokenAmounts: true,
+        outcomeTokenMarginalPric: true
+      }
+    }
+  }
+
+
+
+};
+
+export default class Plugin {
+  public author = 'David';
+  public version = '0.0.1';
+  public name = 'Gnosis Agreements';
+  public website = 'https://gnosis.io';
+  public options: any;
+
+  async action(
+    network,
+    questionId,
+  ) {
+    try {
+      const query = GQL_QUERY;
+      query.question.__args.id = questionId;
+      const result = await subgraphRequest(OMEN_SUBGRAPH_URL[network], query);
+      return result;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
