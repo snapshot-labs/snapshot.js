88e453a35fb9cfa7efa5d21e0dc5a59a40d05d64
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-11-19 20:20:01 +0100

    Add ERC20 Token methods
    - Add ERC20 method and get instrancte from a given contract address.
    - Rename `getPriceImpact` to `getSubgrapInfo`.
    - Replace getSubgrapInfo from questionId to `conditionId` parameter.

--- src/plugins/gnosis/index.ts
+++ src/plugins/gnosis/index.ts
@@ -1,28 +1,70 @@
 import { subgraphRequest } from '../../utils';
 
+const API_ENDPOINT_URL = "https://api.coingecko.com/api/v3/simple/token_price/ethereum"
 const OMEN_SUBGRAPH_URL = {
   '1': 'https://api.thegraph.com/subgraphs/name/protofire/omen',
   '4': 'https://api.thegraph.com/subgraphs/name/protofire/omen-rinkeby'
 };
 
 const GQL_QUERY = {
-  question: {
+  condition: {
     __args: {
       id: undefined
     },
     id: true,
-    title: true,
-    conditions: {
+    fixedProductMarketMakers: {
       id: true,
-      fixedProductMarketMakers: {
-        id: true,
-        collateralToken: true,
-        outcomeTokenAmounts: true,
-        outcomeTokenMarginalPrices: true
-      }
+      collateralToken: true,
+      outcomeTokenAmounts: true,
+      outcomeTokenMarginalPrices: true
     }
   }
 };
+
+const erc20Abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_upgradedAddress","type":"address"}],"name":"deprecate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"deprecated","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_evilUser","type":"address"}],"name":"addBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"upgradedAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balances","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"maximumFee","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_maker","type":"address"}],"name":"getBlackListStatus","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowed","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newBasisPoints","type":"uint256"},{"name":"newMaxFee","type":"uint256"}],"name":"setParams","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"issue","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"redeem","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"basisPointsRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"isBlackListed","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_clearedUser","type":"address"}],"name":"removeBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"MAX_UINT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_blackListedUser","type":"address"}],"name":"destroyBlackFunds","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialSupply","type":"uint256"},{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Issue","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Redeem","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newAddress","type":"address"}],"name":"Deprecate","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"feeBasisPoints","type":"uint256"},{"indexed":false,"name":"maxFee","type":"uint256"}],"name":"Params","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_blackListedUser","type":"address"},{"indexed":false,"name":"_balance","type":"uint256"}],"name":"DestroyedBlackFunds","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"AddedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"RemovedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"}];
+
+/**
+ * Returns the token instance from a give ERC-20 contract address
+ * @param web3 
+ * @param tokenAddress 
+ */
+const getTokenInstance = (web3, tokenAddress) => {
+  return new web3.eth.Contract(erc20Abi, tokenAddress);
+}
+
+/**
+ * Returns the token `symbol` from a given ERC-20 contract instance
+ * @param tokenInstance
+ */
+const getTokenSymbol = async (tokenInstance) => {
+  return tokenInstance.methods.symbol().
+      call().then(symbol => {
+          return symbol;
+      });
+}
+
+/**
+* Returns the token `name` from a given ERC-20 contract instance
+ * @param tokenInstance
+*/
+const getTokenName = async (tokenInstance) => {
+  return tokenInstance.methods.name().
+      call().then(name => {
+          return name;
+      });
+}
+
+/**
+* Returns the token `decimals` from a given ERC-20 contract instance
+* @param web3 the web3 instance.
+* @param tokenAddress ERC-20 contract address.
+*/
+const getTokenDecimals = async (tokenInstance) => {
+  return tokenInstance.methods.decimals().
+      call().then(decimals => {
+          return decimals;
+      });
+}
 
 export default class Plugin {
   public author = 'David';
@@ -31,13 +73,43 @@ export default class Plugin {
   public website = 'https://gnosis.io';
   public options: any;
 
-  async getPriceImpact(
-    network,
-    proposalOptions,
-  ) {
+  async getTokenInfo(web3, tokenAddress) {
+    try {
+      const tokenInstance = getTokenInstance(web3, tokenAddress);
+      return {
+        name: await getTokenName(tokenInstance),
+        symbol: await getTokenSymbol(tokenInstance),
+        decimals: await getTokenDecimals(tokenInstance)
+      }
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  async getTokenPrice(tokenAddress) {
+    return fetch(
+      `${API_ENDPOINT_URL}?contract_addresses=${tokenAddress}&vs_currencies=usd`,
+      { method: 'GET', headers: { 'Content-Type': 'application/json' } }
+    )
+      .then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(json => {
+        if (json.error) {
+          throw new Error(json.error.message);
+        }
+        return {
+          name: json.name,
+          priceRate: json.price.rate,
+          decimals: json.decimals,
+          symbol: json.symbol
+        };
+      });
+  }
+
+  async getSubgrapInfo(network, conditionId) {
     try {
       const query = GQL_QUERY;
-      query.question.__args.id = proposalOptions.questionId;
+      query.condition.__args.id = conditionId;
       const result = await subgraphRequest(OMEN_SUBGRAPH_URL[network], query);
       return result;
     } catch (e) {
