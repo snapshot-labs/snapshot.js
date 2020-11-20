a71e47ca27a0ea202bd4d191966e668e05c28bd2
ncitron <noah@jeff.org>
2020-11-20 18:13:12 -0500

    fixed tests

--- test/index.ts
+++ test/index.ts
@@ -2,34 +2,14 @@ global['fetch'] = require('cross-fetch');
 const { JsonRpcProvider } = require('@ethersproject/providers');
 const snapshot = require('../');
 const networks = require('../src/networks.json');
-const example = require('../src/strategies/erc20-balance-of-with-unclaimed-and-pooled/examples.json')[0];
-//const example = require('../src/strategies/erc20-balance-of/examples.json')[0];
-
-// (async () => {
-//   try {
-//     console.log(example.name);
-//     console.time('getScores');
-//     const scores = await snapshot.utils.getScores(
-//       'yam',
-//       [example.strategy],
-//       example.network,
-//       new JsonRpcProvider(networks[example.network].rpc[0]),
-//       example.addresses,
-//       example.snapshot
-//     );
-//     console.log(scores);
-//     console.timeEnd('getScores');
-//   } catch (e) {
-//     console.error(e);
-//   }
-// })();
+const example = require('../src/strategies/erc20-balance-of/examples.json')[0];
 
 (async () => {
   try {
     console.log(example.name);
     console.time('getScores');
     const scores = await snapshot.utils.getScores(
-      'index',
+      'yam',
       [example.strategy],
       example.network,
       new JsonRpcProvider(networks[example.network].rpc[0]),
