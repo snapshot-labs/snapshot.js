977594c15721cf15184dcedcaf14e08b6cf09efd
pancake-swap <pancakeswap1@protonmail.com>
2020-11-25 05:27:19 +0000

    feat: update pancake strategy

--- src/strategies/pancake/index.ts
+++ src/strategies/pancake/index.ts
@@ -5,7 +5,7 @@ import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
 export const author = 'pancake-swap';
 export const version = '0.0.1';
 
-const abi = [
+const sousChefabi = [
   {
     inputs: [
       {
@@ -27,6 +27,43 @@ const abi = [
   }
 ];
 
+const masterChefAbi = [
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256'
+      },
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address'
+      }
+    ],
+    name: 'userInfo',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256'
+      },
+      {
+        internalType: 'uint256',
+        name: 'rewardDebt',
+        type: 'uint256'
+      }
+    ],
+    stateMutability: 'view',
+    type: 'function'
+  }
+];
+
+
+
+const masterChefContractAddress =
+  '0x73feaa1eE314F8c655E354234017bE2193C9E24E';
+
 export async function strategy(
   space,
   network,
@@ -45,12 +82,24 @@ export async function strategy(
     snapshot
   );
 
-  const balances = await Promise.all(
+  const masterBalance = await multicall(
+    network,
+    provider,
+    masterChefAbi,
+    addresses.map((address: any) => [
+      masterChefContractAddress,
+      'userInfo',
+      ['0', address]
+    ]),
+    { blockTag }
+  );
+
+  const sousBalances = await Promise.all(
     options.chefAddresses.map((chefAddress) =>
       multicall(
         network,
         provider,
-        abi,
+        sousChefabi,
         addresses.map((address: any) => [
           chefAddress,
           'userInfo',
@@ -66,7 +115,10 @@ export async function strategy(
     Object.entries(score).map((address, index) => [
       address[0],
       address[1] +
-        balances.reduce(
+        parseFloat(
+          formatUnits(masterBalance.amount.toString(), 18)
+        ) +
+        sousBalances.reduce(
           (prev: number, cur: any) =>
             prev +
             parseFloat(
