3073b7e37c1332a12953b949d747e4ad4972c8db
pancake-swap <71247357+pancake-swap@users.noreply.github.com>
2020-11-26 03:52:25 +0800

    feat: update pancake strategy (#44)
    * feat: update pancake strategy
    
    * fix: fix merge
    
    * fix: add decimals in specific address
    
    * Update index.ts

--- src/strategies/pancake/index.ts
+++ src/strategies/pancake/index.ts
@@ -5,7 +5,7 @@ import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
 export const author = 'pancake-swap';
 export const version = '0.0.1';
 
-const abi = [
+const sousChefabi = [
   {
     inputs: [
       {
@@ -27,6 +27,43 @@ const abi = [
   }
 ];
 
+const masterChefAbi = [
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256'
+      },
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address'
+      }
+    ],
+    name: 'userInfo',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256'
+      },
+      {
+        internalType: 'uint256',
+        name: 'rewardDebt',
+        type: 'uint256'
+      }
+    ],
+    stateMutability: 'view',
+    type: 'function'
+  }
+];
+
+
+
+const masterChefContractAddress =
+  '0x73feaa1eE314F8c655E354234017bE2193C9E24E';
+
 export async function strategy(
   space,
   network,
@@ -45,14 +82,26 @@ export async function strategy(
     snapshot
   );
 
-  const balances = await Promise.all(
-    options.chefAddresses.map((chefAddress) =>
+  const masterBalance = await multicall(
+    network,
+    provider,
+    masterChefAbi,
+    addresses.map((address: any) => [
+      masterChefContractAddress,
+      'userInfo',
+      ['0', address]
+    ]),
+    { blockTag }
+  );
+
+  const sousBalances = await Promise.all(
+    options.chefAddresses.map((item) =>
       multicall(
         network,
         provider,
-        abi,
+        sousChefabi,
         addresses.map((address: any) => [
-          chefAddress,
+          item.address,
           'userInfo',
           [address],
           { blockTag }
@@ -66,11 +115,20 @@ export async function strategy(
     Object.entries(score).map((address, index) => [
       address[0],
       address[1] +
-        balances.reduce(
-          (prev: number, cur: any) =>
+        masterBalances.reduce(
+          (prev: number, cur: any, idx: number) =>
             prev +
             parseFloat(
-              formatUnits(cur[index].amount.toString(), options.decimals)
+              formatUnits(cur.amount.toString(), 18)
+            ),
+          0
+        )
+        +
+        sousBalances.reduce(
+          (prev: number, cur: any, idx: number) =>
+            prev +
+            parseFloat(
+              formatUnits(cur[index].amount.toString(), options.chefAddresses[idx].decimals)
             ),
           0
         )
