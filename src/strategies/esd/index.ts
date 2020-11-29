a5f0aa2d4e326a7301916e91a90e37cb43437294
Fabien <bonustrack@users.noreply.github.com>
2020-11-29 14:17:19 +0700

    Update src/strategies/esd/index.ts

--- src/strategies/esd/index.ts
+++ src/strategies/esd/index.ts
@@ -1,7 +1,7 @@
 import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
 
-export const author = 'lewi';
+export const author = 'l3wi';
 export const version = '0.1.0';
 
 const abi = [
