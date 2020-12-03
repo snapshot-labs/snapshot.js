82253e074c782500389736986365d3d4d8a21f9f
Felipe Forbeck <felipe.forbeck@gmail.com>
2020-12-03 17:03:29 -0300

    wip

--- src/crypto/index.ts
+++ src/crypto/index.ts
@@ -1,9 +1,13 @@
 const sigUtil = require('eth-sig-util');
-const {keccak} = require('ethereumjs-util');
+const { keccak } = require('ethereumjs-util');
 
 function getMessageERC712Hash(message, verifyingContract, chainId) {
   const m = prepareMessage(message, verifyingContract, chainId);
-  const {DomainType, MessageType} = getDomainType(m, verifyingContract, chainId);
+  const { DomainType, MessageType } = getDomainType(
+    m,
+    verifyingContract,
+    chainId
+  );
   const msgParams = {
     domain: DomainType,
     message: m,
@@ -14,15 +18,15 @@ function getMessageERC712Hash(message, verifyingContract, chainId) {
 }
 
 function getDomainType(message, verifyingContract, chainId) {
-  switch(message.type) {
-    case "vote":
+  switch (message.type) {
+    case 'vote':
       return getVoteDomainType(verifyingContract, chainId);
-    case "proposal":
+    case 'proposal':
       return getProposalDomainType(verifyingContract, chainId);
     default:
-      throw new Error("unknown type " + message.type);
+      throw new Error('unknown type ' + message.type);
   }
- }
+}
 
 function getVoteDomainType(verifyingContract, chainId) {
   const DomainType = {
@@ -34,20 +38,22 @@ function getVoteDomainType(verifyingContract, chainId) {
 
   // The named list of all type definitions
   const MessageType = {
-      Message: [
-        { name: 'versionHash', type: 'bytes32' },
-        { name: 'timestamp', type: 'uint256' },
-        { name: 'spaceHash', type: 'bytes32' },
-        { name: 'payload', type: 'MessagePayload' }
-      ],
-      MessagePayload: [
-        { name: 'choice', type: 'uint256' },
-        { name: 'proposalHash', type: 'bytes32' },
-        { name: 'metadataHash', type: 'bytes32' }
-      ]
+    Message: [
+      { name: 'versionHash', type: 'string' },
+      { name: 'timestamp', type: 'uint256' },
+      { name: 'spaceHash', type: 'string' },
+      { name: 'payload', type: 'MessagePayload' },
+      { name: 'token', type: 'string' },
+      { name: 'type', type: 'string' }
+    ],
+    MessagePayload: [
+      { name: 'choice', type: 'uint256' },
+      { name: 'proposal', type: 'string' },
+      { name: 'metadataHash', type: 'string' }
+    ]
   };
 
-  return { DomainType, MessageType}
+  return { DomainType, MessageType };
 }
 
 function getProposalDomainType(verifyingContract, chainId) {
@@ -59,109 +65,143 @@ function getProposalDomainType(verifyingContract, chainId) {
   };
 
   const MessageType = {
-      Message: [
-        { name: 'versionHash', type: 'bytes32' },
-        { name: 'timestamp', type: 'uint256' },
-        { name: 'spaceHash', type: 'bytes32' },
-        { name: 'payload', type: 'MessagePayload' }
-      ],
-      MessagePayload: [
-        { name: 'nameHash', type: 'bytes32' },
-        { name: 'bodyHash', type: 'bytes32' },
-        { name: 'choices', type: 'string[]' },
-        { name: 'start', type: 'uint256' },
-        { name: 'end', type: 'uint256' },
-        { name: 'snapshot', type: 'uint256' },
-        { name: 'metadataHash', type: 'bytes32' }
-      ]
+    Message: [
+      { name: 'versionHash', type: 'string' },
+      { name: 'timestamp', type: 'uint256' },
+      { name: 'spaceHash', type: 'string' },
+      { name: 'payload', type: 'MessagePayload' },
+      { name: 'token', type: 'string' },
+      { name: 'type', type: 'string' }
+    ],
+    MessagePayload: [
+      { name: 'nameHash', type: 'string' },
+      { name: 'bodyHash', type: 'string' },
+      { name: 'choices', type: 'string[]' },
+      { name: 'start', type: 'uint256' },
+      { name: 'end', type: 'uint256' },
+      { name: 'snapshot', type: 'uint256' },
+      { name: 'metadataHash', type: 'string' }
+    ]
   };
 
-  return { DomainType, MessageType}
+  return { DomainType, MessageType };
 }
 
 async function signMessage(signer, message, verifyingContract, chainId) {
+  console.log('Type: ', message.type);
   return signer(message, verifyingContract, chainId);
 }
 
-function validateMessage(message, address, verifyingContract, chainId, signature ) {
-  const {DomainType, MessageType} = getDomainType(message, verifyingContract, chainId); 
-  
+function validateMessage(
+  message,
+  address,
+  verifyingContract,
+  chainId,
+  signature
+) {
+  const { DomainType, MessageType } = getDomainType(
+    message,
+    verifyingContract,
+    chainId
+  );
+
   const msgParams = {
     domain: DomainType,
     message,
     primaryType: 'Message',
     types: MessageType
   };
-  
-  const recoverAddress = sigUtil.recoverTypedSignature_v4({ data: msgParams, sig: signature })
+
+  const recoverAddress = sigUtil.recoverTypedSignature_v4({
+    data: msgParams,
+    sig: signature
+  });
   return address.toLowerCase() === recoverAddress.toLowerCase();
 }
 
 function Web3Signer(web3) {
-  return function(message, verifyingContract, chainId) {
+  return function (message, verifyingContract, chainId) {
     const m = prepareMessage(message, verifyingContract, chainId);
-    const {DomainType, MessageType} = getDomainType(m, verifyingContract, chainId);
+    const { DomainType, MessageType } = getDomainType(
+      m,
+      verifyingContract,
+      chainId
+    );
     const signer = web3.getSigner();
-    
+
     return signer._signTypedData(DomainType, MessageType, m);
-  }
+  };
 }
 
 function SigUtilSigner(privateKeyStr) {
-  return function(message, verifyingContract, chainId) {
+  return function (message, verifyingContract, chainId) {
+    console.log('Sign:', message.type);
     const m = prepareMessage(message, verifyingContract, chainId);
     const privateKey = Buffer.from(privateKeyStr, 'hex');
-    const {DomainType, MessageType} = getDomainType(m, verifyingContract, chainId);
+    const { DomainType, MessageType } = getDomainType(
+      m,
+      verifyingContract,
+      chainId
+    );
     const msgParams = {
       domain: DomainType,
       message: m,
       primaryType: 'Message',
       types: MessageType
     };
-    return sigUtil.signTypedData_v4(privateKey, {data: msgParams});
-  }
+    return sigUtil.signTypedData_v4(privateKey, { data: msgParams });
+  };
 }
 
 function prepareMessage(message, verifyingContract, chainId) {
-  switch(message.type) {
-    case "vote":
+  console.log('Prepara: ', JSON.stringify(message));
+  switch (message.type) {
+    case 'vote':
       return prepareVoteMessage(message, verifyingContract, chainId);
-    case "proposal":
+    case 'proposal':
       return prepareProposalMessage(message);
     default:
-      throw new Error("unknown type " + message.type);
+      throw new Error('unknown type ' + message.type);
   }
 }
 
 function prepareVoteMessage(message, verifyingContract, chainId) {
   return Object.assign(message, {
-    versionHash: keccak(message.version),
-    spaceHash: keccak(message.space),
+    versionHash: hexKeccak(message.version),
+    spaceHash: hexKeccak(message.space),
     payload: prepareVotePayload(message.payload, verifyingContract, chainId)
   });
 }
 
 function prepareVotePayload(payload, verifyingContract, chainId) {
   return Object.assign(payload, {
-      metadataHash: keccak(JSON.stringify(payload.metadata)),
-      proposalHash: getMessageERC712Hash(payload.proposal, verifyingContract, chainId)
+    metadataHash: hexKeccak(JSON.stringify(payload.metadata)),
+    proposalHash: getMessageERC712Hash(
+      payload.proposal,
+      verifyingContract,
+      chainId
+    )
   });
 }
 
 function prepareProposalMessage(message) {
   return Object.assign(message, {
-    versionHash: keccak(message.version),
-    spaceHash: keccak(message.space),
+    versionHash: hexKeccak(message.version),
+    spaceHash: hexKeccak(message.space),
     payload: prepareProposalPayload(message.payload)
   });
 }
 
 function prepareProposalPayload(payload) {
   return Object.assign(payload, {
-      nameHash: keccak(payload.name),
-      bodyHash: keccak(payload.body),
-      metadataHash: keccak(JSON.stringify(payload.metadata))
+    nameHash: hexKeccak(payload.name),
+    bodyHash: hexKeccak(payload.body),
+    metadataHash: hexKeccak(JSON.stringify(payload.metadata))
   });
+}
+
+function hexKeccak(obj) {
+  return keccak(obj).toString('hex');
 }
 
 module.exports = {
--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,14 @@
-
 const { JsonRpcProvider } = require('@ethersproject/providers');
 const snapshot = require('../');
 const networks = require('../src/networks.json');
 const example = require('../src/strategies/erc20-balance-of/examples.json')[0];
-const {signMessage, validateMessage, SigUtilSigner, getDomainType, getMessageERC712Hash} = require('../src/crypto/index.ts');
+const {
+  signMessage,
+  validateMessage,
+  SigUtilSigner,
+  getDomainType,
+  getMessageERC712Hash
+} = require('../src/crypto/index.ts');
 const sigUtil = require('eth-sig-util');
 
 (async () => {
@@ -30,49 +35,124 @@ const sigUtil = require('eth-sig-util');
 (async () => {
   try {
     console.log('start signature ...');
-  
-    const proposalMessage = {
-      "payload":{
-        "name":"Example Project",
-        "body":"xyz\n\nasdasd\n\nasdasd",
-        "choices":["Yes","No"],
-        "start":1605099037,
-        "end":1605131437,
-        "snapshot":7529379,
-        "metadata":{"uuid":"c51c5424-ea9d-4498-b812-af41da595827"}
-      },
-      "timestamp":1605099037,
-      "space":"myspace",
-      "type":"proposal",
-      "version":"0.1.3"
-    }
-
-    const voteMessage = {
-      "payload":{
-        "choice":1,
-        "proposalIpfs": "QmcHcqAAz81aaBLtYfepSJGkbSqTfchMs1Qp8TdzMKp9DN",
-        "proposal":proposalMessage,
-        "metadata":{
-          "memberAddress":"0xDe6ab16a4015c680daab58021815D09ddB57db8E"
-        }
-      },
-      timestamp:1605099139,
-      space:"myspace",
-      type:"vote",
-      version:"0.1.3"
-    }
 
     const chainId = 5777;
     const verifyingContract = '0xcFc2206eAbFDc5f3d9e7fA54f855A8C15D196c05';
-    const voteSignature = await signMessage(SigUtilSigner('7e91fc4c3424c0594078bcd9c80a7f788ec345e77254e50d3e197e9396e0c472'), voteMessage, verifyingContract, chainId);
-    const isSignatureValid = validateMessage(voteMessage, '0x3098C683320703B2B0922f7a2CE67D2ee321EaA9', verifyingContract, chainId, voteSignature);
-    console.log('vote validation:', isSignatureValid);
+    const proposalIpfs = 'QmcHcqAAz81aaBLtYfepSJGkbSqTfchMs1Qp8TdzMKp9DN';
+    const address = '0x3098C683320703B2B0922f7a2CE67D2ee321EaA9';
+    const timestamp = (Date.now() / 1e3).toFixed();
 
+    const proposalMessage = {
+      address: address,
+      msg: {
+        payload: {
+          name: 'Example Project',
+          body: 'xyz\n\nasdasd\n\nasdasd',
+          choices: ['Yes', 'No'],
+          start: 1605099037,
+          end: 1605131437,
+          snapshot: 7529379,
+          metadata: {
+            uuid: 'c51c5424-ea9d-4498-b812-af41da595827',
+            private: false,
+            type: 'Governance',
+            subType: 'Governance'
+          }
+        },
+        timestamp: timestamp,
+        token: '0x8f56682a50becb1df2fb8136954f2062871bc7fc',
+        space: 'myspace',
+        type: 'proposal',
+        version: '0.1.3',
+        chainId: chainId,
+        verifyingContract: verifyingContract
+      }
+    };
 
-    const proposalSignature = await signMessage(SigUtilSigner('7e91fc4c3424c0594078bcd9c80a7f788ec345e77254e50d3e197e9396e0c472'), proposalMessage, verifyingContract, chainId);
-    const isProposalValid = validateMessage(proposalMessage, '0x3098C683320703B2B0922f7a2CE67D2ee321EaA9', verifyingContract, chainId, proposalSignature);
-    console.log('proposal hash:', getMessageERC712Hash(proposalMessage, verifyingContract, chainId));
+    const proposalSignature = await signMessage(
+      SigUtilSigner(
+        '7e91fc4c3424c0594078bcd9c80a7f788ec345e77254e50d3e197e9396e0c472'
+      ),
+      proposalMessage.msg,
+      proposalMessage.msg.verifyingContract,
+      proposalMessage.msg.chainId
+    );
+    const newMsg = {
+      ...proposalMessage,
+      sig: proposalSignature,
+      msg: JSON.stringify(proposalMessage.msg)
+    };
+
+    console.log('[msg][proposal] => ', JSON.stringify(newMsg));
+    const isProposalValid = validateMessage(
+      proposalMessage.msg,
+      proposalMessage.address,
+      proposalMessage.msg.verifyingContract,
+      proposalMessage.msg.chainId,
+      proposalSignature
+    );
+    console.log(
+      'proposal hash:',
+      getMessageERC712Hash(
+        proposalMessage.msg,
+        proposalMessage.msg.verifyingContract,
+        proposalMessage.msg.chainId
+      )
+    );
     console.log('proposal validation:', isProposalValid);
+
+    const voteMessage = {
+      address: address,
+      msg: {
+        payload: {
+          choice: 1,
+          proposal: proposalIpfs, //we dont need to pass the entire proposal obj
+          metadata: {
+            memberAddress: '0xDe6ab16a4015c680daab58021815D09ddB57db8E'
+          }
+        },
+        timestamp: timestamp,
+        token: '0x8f56682a50becb1df2fb8136954f2062871bc7fc',
+        space: 'myspace',
+        type: 'vote',
+        version: '0.1.3',
+        chainId: chainId,
+        verifyingContract: verifyingContract
+      }
+    };
+
+    const voteMsg = { ...voteMessage.msg };
+
+    const voteSignature = await signMessage(
+      SigUtilSigner(
+        '7e91fc4c3424c0594078bcd9c80a7f788ec345e77254e50d3e197e9396e0c472'
+      ),
+      voteMsg,
+      voteMsg.verifyingContract,
+      voteMsg.chainId
+    );
+    const isVoteValid = validateMessage(
+      voteMessage.msg,
+      voteMessage.address,
+      voteMessage.msg.verifyingContract,
+      voteMessage.msg.chainId,
+      voteSignature
+    );
+    const newVoteMsg = {
+      ...voteMessage,
+      sig: voteSignature,
+      msg: JSON.stringify(voteMessage.msg)
+    };
+    console.log('[msg][vote] => ', JSON.stringify(newVoteMsg));
+    console.log(
+      'vote hash:',
+      getMessageERC712Hash(
+        voteMessage.msg,
+        voteMessage.msg.verifyingContract,
+        voteMessage.msg.chainId
+      )
+    );
+    console.log('vote validation:', isVoteValid);
   } catch (e) {
     console.error(e);
   }
