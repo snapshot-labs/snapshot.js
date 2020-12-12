2897b2cf803723bb465a4d70f79553353329ee76
sayli <ptsayli@gmail.com>
2020-12-13 02:03:23 +0530

    Add: defi-dollar strategy

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,24 +1,26 @@
 import { strategy as balancer } from './balancer';
 import { strategy as contractCall } from './contract-call';
+import { strategy as cream } from './cream';
+import { strategy as ctoken } from './ctoken';
+import { strategy as defiDollar } from './defi-dollar';
 import { strategy as erc20BalanceOf } from './erc20-balance-of';
 import { strategy as erc20BalanceOfCoeff } from './erc20-balance-of-coeff';
-import { strategy as erc20BalanceOfFixedTotal } from './erc20-balance-of-fixed-total';
 import { strategy as erc20BalanceOfCv } from './erc20-balance-of-cv';
-import { strategy as erc20WithBalance } from './erc20-with-balance';
 import { strategy as erc20BalanceOfDelegation } from './erc20-balance-of-delegation';
+import { strategy as erc20BalanceOfFixedTotal } from './erc20-balance-of-fixed-total';
+import { strategy as erc20WithBalance } from './erc20-with-balance';
+import { strategy as esd } from './esd';
 import { strategy as ethBalance } from './eth-balance';
 import { strategy as makerDsChief } from './maker-ds-chief';
-import { strategy as uni } from './uni';
-import { strategy as yearnVault } from './yearn-vault';
 import { strategy as moloch } from './moloch';
-import { strategy as uniswap } from './uniswap';
 import { strategy as pancake } from './pancake';
-import { strategy as synthetix } from './synthetix';
-import { strategy as ctoken } from './ctoken';
-import { strategy as cream } from './cream';
-import { strategy as esd } from './esd';
-import { strategy as stakedUniswap } from './staked-uniswap';
 import { strategy as piedao } from './piedao';
+import { strategy as stakedUniswap } from './staked-uniswap';
+import { strategy as synthetix } from './synthetix';
+import { strategy as uni } from './uni';
+import { strategy as uniswap } from './uniswap';
+import { strategy as yearnVault } from './yearn-vault';
+
 
 export default {
   balancer,
@@ -41,5 +43,6 @@ export default {
   cream,
   'staked-uniswap': stakedUniswap,
   esd,
-  piedao
+  piedao,
+  'defi-dollar': defiDollar
 };
--- test/index.ts
+++ test/index.ts
@@ -2,21 +2,22 @@ global['fetch'] = require('cross-fetch');
 const { JsonRpcProvider } = require('@ethersproject/providers');
 const snapshot = require('../');
 const networks = require('../src/networks.json');
-const example = require('../src/strategies/erc20-balance-of/examples.json')[0];
+const example = require('../src/strategies/defi-dollar/examples.json')[0];
 
 (async () => {
   try {
+    console.log("welcome")
     console.log(example.name);
     console.time('getScores');
     const scores = await snapshot.utils.getScores(
-      'yam',
+      'defi-dollar',
       [example.strategy],
       example.network,
       new JsonRpcProvider(networks[example.network].rpc[0]),
       example.addresses,
       example.snapshot
     );
-    console.log(scores);
+    console.log("scores",  scores);
     console.timeEnd('getScores');
   } catch (e) {
     console.error(e);
