19f3ee3c61b9711bd1c258c84fc230beb48a512b
atvanguard <93arpit@gmail.com>
2020-12-18 23:22:29 +0530

    Add unstaked LP balance

--- src/strategies/defidollar/index.ts
+++ src/strategies/defidollar/index.ts
@@ -92,19 +92,20 @@ export async function strategy(
   const queries = []
 
   // curve farm needs special handling
-  for (let j = 0; j < addresses.length; j++) {
-    queries.push([options.farms.curve.farm, 'claimable_reward', [addresses[j]]])
-  }
+  addresses.forEach(voter => {
+    queries.push([options.farms.curve.farm, 'claimable_reward', [voter]])
+  })
 
   const farms = Object.keys(options.farms).slice(1) // exclude curve
   for (let i = 0; i < farms.length; i++) {
     const { farm, lpToken } = options.farms[farms[i]]
     queries.push([options.DFD, 'balanceOf', [lpToken]])
     queries.push([lpToken, 'totalSupply'])
-    for (let j = 0; j < addresses.length; j++) {
-      queries.push([farm, 'balanceOf', [addresses[j]]])
-      queries.push([farm, 'earned', [addresses[j]]])
-    }
+    addresses.forEach(voter => {
+      queries.push([farm, 'balanceOf', [voter]])
+      queries.push([lpToken, 'balanceOf', [voter]])
+      queries.push([farm, 'earned', [voter]])
+    })
   }
 
   let response = await multicall(
@@ -127,15 +128,18 @@ export async function strategy(
         let score = curveEarned[i]
 
         while(response.length) {
-          const res = response.slice(0, 2 * (n+1)).map(r => r[0]) // 2(n+1) queries for each farm
-          response = response.slice(2 * (n+1))
+          const res = response.slice(0, 2 + 3 * n).map(r => r[0]) // 2 + 3n queries for each farm
+          response = response.slice(2 + 3 * n)
           /*
-            staked = (dfd.balanceOf(lpToken) * farm.balanceOf(user) / lpToken.totalSupply()
+            lpTokenBalance = farm.balanceOf(user) + lpToken.balanceOf(user)
+            staked = (dfd.balanceOf(lpToken) * lpTokenBalance) / lpToken.totalSupply()
             earned = farm.earned(user)
             score = staked + earned
           */
-          const staked = res[0].mul(res[2 + 2 * i]).div(res[1])
-          const earned = res[2 + (2 * i + 1)]
+          const start = 2 + 3 * i
+          const lpTokenBalance = res[start].add(res[start + 1])
+          const staked = res[0].mul(lpTokenBalance).div(res[1])
+          const earned = res[start + 2]
           score = score.add(staked).add(earned)
         }
 
