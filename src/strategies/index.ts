063568648479a051eb1c5bd81c54a6d713fbbe5e
Arpit Agarwal <93arpit@gmail.com>
2020-12-20 11:06:51 +0530

    DefiDollar Strategy (#62)
    * defidollar-strategy
    
    * Add unstaked LP balance
    
    * Revert test and lint

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -22,6 +22,7 @@ import { strategy as esdDelegation } from './esd-delegation';
 import { strategy as stakedUniswap } from './staked-uniswap';
 import { strategy as piedao } from './piedao';
 import { strategy as xDaiStake } from './xdai-stake';
+import { strategy as defidollar } from './defidollar';
 
 export default {
   balancer,
@@ -47,5 +48,6 @@ export default {
   esd,
   'esd-delegation': esdDelegation,
   piedao,
-  'xdai-stake': xDaiStake
+  'xdai-stake': xDaiStake,
+  defidollar
 };
