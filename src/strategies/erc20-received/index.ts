05b45d0e68fac7d0e33bd90b71ac124133fc5e4e
mccallofthewild <mccallalexander96@gmail.com>
2020-12-26 12:31:08 -0700

    refactored erc20-received to use scalable Dfuse GraphQL subscription

--- src/strategies/erc20-received/index.ts
+++ src/strategies/erc20-received/index.ts
@@ -2,6 +2,18 @@ import fetch from 'cross-fetch';
 import { InfuraProvider, Web3Provider } from '@ethersproject/providers';
 import { formatUnits } from '@ethersproject/units';
 import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
+import { createDfuseClient } from '@dfuse/client';
+
+// @ts-ignore
+if (!global.WebSocket) {
+  // @ts-ignore
+  global.WebSocket = require('ws');
+}
+
+const client = createDfuseClient({
+  apiKey: 'server_806bdc9bb370dad11ec5807e82e57fa0',
+  network: 'mainnet.eth.dfuse.io'
+});
 
 export const author = 'mccallofthewild';
 export const version = '0.1.0';
@@ -24,37 +36,96 @@ export async function strategy(
   const [space, network, provider, addresses, options, snapshot] = args;
   const { coeff = 1, receivingAddresses, contractAddress, decimals } = options;
 
-  const logResults = await new InfuraProvider().getLogs({
-    address: contractAddress,
-    fromBlock: 0,
-    toBlock: 'latest',
-    topics: [
-      // transfer
-      '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
-      // from
-      addresses.map((a) => a.replace('0x', '0x000000000000000000000000')),
-      // to
-      receivingAddresses.map((a) =>
-        a.replace('0x', '0x000000000000000000000000')
-      )
-    ]
+  type TokenTxLog = {
+    from: string;
+    to: string;
+    amount: string;
+  };
+  const txLogs: TokenTxLog[] = await new Promise(async (resolve, reject) => {
+    const txs: TokenTxLog[] = [];
+    client.graphql(
+      /* GraphQL */ `
+        subscription(
+          $query: String!
+          $sort: SORT
+          $low: Int64
+          $high: Int64
+          $limit: Int64
+        ) {
+          searchTransactions(
+            indexName: LOGS
+            query: $query
+            sort: $sort
+            lowBlockNum: $low
+            highBlockNum: $high
+            limit: $limit
+          ) {
+            cursor
+            node {
+              matchingLogs {
+                topics
+                data
+              }
+            }
+          }
+        }
+      `,
+      (message, stream) => {
+        if (message.type === 'error') {
+          console.log('An error occurred', message.errors, message.terminal);
+        }
+
+        if (message.type === 'data') {
+          const {
+            cursor,
+            node: { matchingLogs }
+          } = message.data.searchTransactions;
+          txs.push(
+            ...matchingLogs.map((log) => {
+              const [, from, to] = log.topics.map((t) =>
+                t.replace('0x000000000000000000000000', '0x')
+              );
+              const amount = BigNumber.from(log.data);
+              return {
+                from,
+                to,
+                amount
+              };
+            })
+          );
+          stream.mark({ cursor });
+        }
+
+        if (message.type === 'complete') {
+          resolve(txs);
+        }
+      },
+      {
+        variables: {
+          query: `topic.0:'Transfer(address,address,uint256)' (${addresses
+            .map((a) => `topic.1:'${a}'`)
+            .join(' OR ')}) (${receivingAddresses
+            .map((a) => `topic.2:'${a}'`)
+            .join(' OR ')})`,
+          sort: 'ASC',
+          limit: 0,
+          high: await provider.getBlockNumber()
+        }
+      }
+    );
   });
   const scores = {};
   for (const address of addresses) {
-    const logsWithAddress = logResults.filter(
-      ({ topics: [, fromHex, toHex] }) => {
-        const validAddress =
-          fromHex.replace('0x000000000000000000000000', '0x').toLowerCase() ==
-          address.toLowerCase();
-        return validAddress;
-      }
-    );
+    const logsWithAddress = txLogs.filter((log) => {
+      const validAddress = log.from.toLowerCase() == address.toLowerCase();
+      return validAddress;
+    });
     // Sum of all transfers
     scores[address] = logsWithAddress.reduce((prev, curr) => {
       return (
         prev +
         parseFloat(
-          formatUnits(BigNumber.from(curr.data), BigNumber.from(decimals))
+          formatUnits(BigNumber.from(curr.amount), BigNumber.from(decimals))
         ) *
           coeff
       );
