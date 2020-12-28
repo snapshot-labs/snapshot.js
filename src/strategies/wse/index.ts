b117ca5ef84268cf317bd4f4eb54e300f74bfadf
Nick <nick@MacBook-Pro-Nick.local>
2020-12-28 13:31:20 +0200

    Add wse strategy

---
+++ src/strategies/wse/index.ts
@@ -0,0 +1,61 @@
+import { formatUnits } from '@ethersproject/units';
+import { multicall } from '../../utils';
+
+export const author = 'bonustrack';
+export const version = '0.1.0';
+
+const WSE_ADDRESS = {
+  '1': '0x77b8ae2E83c7d044d159878445841E2A9777Af38'
+};
+
+const abi = [
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'account',
+        type: 'address'
+      }
+    ],
+    name: 'getCurrentVotes',
+    outputs: [
+      {
+        internalType: 'uint96',
+        name: '',
+        type: 'uint96'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  }
+];
+
+export async function strategy(
+  space,
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+) {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+  const response = await multicall(
+    network,
+    provider,
+    abi,
+    addresses.map((address: any) => [
+      WSE_ADDRESS[network],
+      'getCurrentVotes',
+      [address.toLowerCase()],
+      { blockTag }
+    ])
+  );
+  return Object.fromEntries(
+    response.map((value, i) => [
+      addresses[i],
+      parseFloat(formatUnits(value.toString(), options.decimals))
+    ])
+  );
+}
