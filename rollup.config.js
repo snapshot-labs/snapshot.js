7b93bfb4d482f6d5447a546585ea23bd28552c84
Chaitanya <yourchaitu@gmail.com>
2020-12-28 18:02:14 +0530

    Fix for packages  undefined error

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import globals from 'rollup-plugin-node-globals';
+import builtins from 'rollup-plugin-node-builtins';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import filesize from 'rollup-plugin-filesize';
 import json from 'rollup-plugin-json';
 import pkg from './package.json';
@@ -11,7 +15,7 @@ const external = [...Object.keys(pkg.dependencies || {})];
 export default [
   {
     input,
-    external,
+    context: 'window',
     output: {
       name,
       file: pkg.browser,
@@ -19,7 +23,11 @@ export default [
     },
     plugins: [
       json(),
+      builtins(),
       typescript({ clean: true }),
+      nodeResolve({ preferBuiltins: true, browser: true }),
+      commonjs(),
+      globals(),
       terser(),
       filesize()
     ]
