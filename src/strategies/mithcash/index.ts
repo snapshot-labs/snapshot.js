fbde0b473fa60e37902459df10410a4c788acdc8
Jeremy Yang <jeremy@cream.finance>
2021-01-07 14:56:28 +0800

    Remove unused abi

--- src/strategies/mithcash/index.ts
+++ src/strategies/mithcash/index.ts
@@ -7,33 +7,16 @@ export const version = '0.1.0';
 const abi = [
   {
     constant: true,
-    inputs: [{ internalType: 'address', name: '', type: 'address' }],
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address'
+      }
+    ],
     name: 'balanceOf',
     outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     payable: false,
-    stateMutability: 'view',
-    type: 'function'
-  },
-  {
-    inputs: [],
-    name: 'getReserves',
-    outputs: [
-      {
-        internalType: 'uint112',
-        name: '_reserve0',
-        type: 'uint112'
-      },
-      {
-        internalType: 'uint112',
-        name: '_reserve1',
-        type: 'uint112'
-      },
-      {
-        internalType: 'uint32',
-        name: '_blockTimestampLast',
-        type: 'uint32'
-      }
-    ],
     stateMutability: 'view',
     type: 'function'
   },
