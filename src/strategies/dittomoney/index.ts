37bea78e56d644305a953f52586eda6bb19b986d
codingsh <codingsh@pm.me>
2021-01-10 22:17:32 +0000

    chore(strategy/ditto): change multicall to Multicaller

--- src/strategies/dittomoney/index.ts
+++ src/strategies/dittomoney/index.ts
@@ -1,5 +1,5 @@
 import { formatUnits, parseUnits } from '@ethersproject/units';
-import { multicall } from '../../utils';
+import Multicaller from '../../utils/multicaller';
 
 export const author = 'codingsh';
 export const version = '0.1.0';
@@ -32,6 +32,21 @@ const abi = [
     ],
     stateMutability: 'view',
     type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'addr',
+        type: 'address'
+      }
+    ],
+    name: 'totalStakedFor',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
   }
 ];
 
@@ -45,58 +60,38 @@ export async function strategy(
 ) {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
-  const response = await multicall(
-    network,
-    provider,
-    abi,
-    [
-      [options.token,'balanceOf',[options.pancake]],
-      [[options.pancake],'totalSupply'],
-      ...addresses.map((address: any) => [
-        options.pancake,
-        'balanceOf',
-        [address]
-      ]),
-      ...addresses.map((address: any) => [
-        options.sharePool,
-        'balanceOf',
-        [address]
-      ]),
-      ...addresses.map((address: any) => [
-        options.token,
-        'balanceOf',
-        [address]
-      ])
-    ],
-    { blockTag }
-  );
+  const multi = new Multicaller(network, provider, abi, options);
 
-  const dittoPerLP = parseUnits(response[0][0].toString(), 18).div(
-    response[1][0]
-  );
-  const lpBalances = response.slice(2, addresses.length + 2);
-  const stakedLpBalances = response.slice(
-    addresses.length + 2,
-    addresses.length * 2 + 2
-  );
-  const tokenBalances = response.slice(
-    addresses.length * 2 + 2,
-    addresses.length * 3 + 2
-  );
+  multi.call('pancakeBalance', options.token, 'balanceOf', [options.pancake]);
+  multi.call('pancakeTotalSupply', options.pancake, 'totalSupply');
+  addresses.forEach(address => {
+    multi.call(`scores.${address}.totalStaked`, options.sharePool, 'totalStakedFor', [address]);
+    multi.call(`scores.${address}.pancake`, options.pancake, 'balanceOf', [address]);
+    multi.call(`scores.${address}.balance`, options.token, 'balanceOf', [address]);
+  });
+
+
+  const result = await multi.execute();
+
+  const dittoPerLP = result.pancakeBalance;
+  const lpBalances = result.scores[0].pancake;
+  const stakedLpBalances = result.scores[0].totalStaked;
+  const tokenBalances = result.scores[0].balance;
+
 
   return Object.fromEntries(
     Array(addresses.length)
       .fill('')
       .map((_, i) => {
-        const lpBalance = lpBalances[i][0].add(stakedLpBalances[i][0]);
+        const lpBalance = lpBalances[i].add(stakedLpBalances[i]);
         const dittoLpBalance = lpBalance.mul(dittoPerLP).div(parseUnits('1', 18));
 
         return [
           addresses[i],
           parseFloat(
             formatUnits(
-              dittoLpBalance
-                .add(tokenBalances[i][0]),
+              dittoLpBalance[i]
+                .add(tokenBalances[i]),
               options.decimals
             )
           )
