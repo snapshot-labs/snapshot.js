68f5d161c9028a5eafee673373319c9520bfb903
PencilDad <pencildad5@gmail.com>
2021-01-25 00:21:11 +0800

    Change to Multicaller

--- src/strategies/typhoon/index.ts
+++ src/strategies/typhoon/index.ts
@@ -1,5 +1,5 @@
 import { formatUnits, parseUnits } from '@ethersproject/units';
-import { multicall } from '../../utils';
+import Multicaller from '../../utils/multicaller';
 
 export const author = 'PencilDad';
 export const version = '0.1.0';
@@ -45,80 +45,52 @@ export async function strategy(
 ) {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
-  const response = await multicall(
-    network,
-    provider,
-    abi,
-    [
-      [options.token, 'balanceOf', [options.micLP]],
-      [options.micLP, 'totalSupply'],
-      [options.token, 'balanceOf', [options.usdtLP]],
-      [options.usdtLP, 'totalSupply'],
-      ...addresses.map((address: any) => [
-        options.micLP,
-        'balanceOf',
-        [address]
-      ]),
-      ...addresses.map((address: any) => [
-        options.usdtLP,
-        'balanceOf',
-        [address]
-      ]),
-      ...addresses.map((address: any) => [
-        options.micRewardPool,
-        'balanceOf',
-        [address]
-      ]),
-      ...addresses.map((address: any) => [
-        options.usdtRewardPool,
-        'balanceOf',
-        [address]
-      ]),
-      ...addresses.map((address: any) => [
-        options.token,
-        'balanceOf',
-        [address]
-      ])
-    ],
-    { blockTag }
-  );
+  const multi = new Multicaller(network, provider, abi, { blockTag });
+  multi.call('micLP.phoon', options.token, 'balanceOf', [options.micLP]);
+  multi.call('micLP.totalSupply', options.micLP, 'totalSupply');
+  multi.call('usdtLP.phoon', options.token, 'balanceOf', [options.usdtLP]);
+  multi.call('usdtLP.totalSupply', options.usdtLP, 'totalSupply');
+  addresses.forEach((address) => {
+    multi.call(`balance.${address}`, options.token, 'balanceOf', [address]);
+    multi.call(`micLP.${address}.balance`, options.micLP, 'balanceOf', [
+      address
+    ]);
+    multi.call(`micLP.${address}.staked`, options.micRewardPool, 'balanceOf', [
+      address
+    ]);
+    multi.call(`usdtLP.${address}.balance`, options.usdtLP, 'balanceOf', [
+      address
+    ]);
+    multi.call(
+      `usdtLP.${address}.staked`,
+      options.usdtRewardPool,
+      'balanceOf',
+      [address]
+    );
+  });
+  const result = await multi.execute();
 
-  const phoonPerMicLP = parseUnits(response[0][0].toString(), 18).div(
-    response[1][0]
+  const phoonPerMicLP = parseUnits(result.micLP.phoon.toString(), 18).div(
+    result.micLP.totalSupply
   );
-  const phoonPerUsdtLP = parseUnits(response[2][0].toString(), 18).div(
-    response[3][0]
-  );
-  const micLPBalances = response.slice(4, addresses.length + 4);
-  const usdtLPBalances = response.slice(
-    addresses.length + 4,
-    addresses.length * 2 + 4
-  );
-  const micLPStakedBalances = response.slice(
-    addresses.length * 2 + 4,
-    addresses.length * 3 + 4
-  );
-  const usdtLPStakedBalances = response.slice(
-    addresses.length * 3 + 4,
-    addresses.length * 4 + 4
-  );
-  const tokenBalances = response.slice(
-    addresses.length * 4 + 4,
-    addresses.length * 5 + 4
+  const phoonPerUsdtLP = parseUnits(result.usdtLP.phoon.toString(), 18).div(
+    result.usdtLP.totalSupply
   );
 
   return Object.fromEntries(
-    tokenBalances.map((tokenBalance, i) => {
-      const micLPBalance = micLPBalances[i][0].add(micLPStakedBalances[i][0]);
-      const usdtLPBalance = usdtLPBalances[i][0].add(
-        usdtLPStakedBalances[i][0]
-      );
-      const score = micLPBalance
-        .mul(phoonPerMicLP)
-        .add(usdtLPBalance.mul(phoonPerUsdtLP))
-        .div(parseUnits('1', 18))
-        .add(tokenBalance[0]);
-      return [addresses[i], parseFloat(formatUnits(score, 18))];
-    })
+    Array(addresses.length)
+      .fill('')
+      .map((_, i) => {
+        const micPhoon = result.micLP[addresses[i]].balance
+          .add(result.micLP[addresses[i]].staked)
+          .mul(phoonPerMicLP)
+          .div(parseUnits('1', 18));
+        const usdtPhoon = result.usdtLP[addresses[i]].balance
+          .add(result.usdtLP[addresses[i]].staked)
+          .mul(phoonPerUsdtLP)
+          .div(parseUnits('1', 18));
+        const score = result.balance[addresses[i]].add(micPhoon).add(usdtPhoon);
+        return [addresses[i], parseFloat(formatUnits(score, 18))];
+      })
   );
 }
