1734bbfa9a0ce55fe541cf77cb92d2455dcfad2c
corollari <github@albert.sh>
2021-01-24 18:00:42 +0100

    Make keep-staked multi-call based

--- src/strategies/staked-keep/index.ts
+++ src/strategies/staked-keep/index.ts
@@ -1,17 +1,46 @@
 import { getAddress } from '@ethersproject/address';
+import { formatUnits } from '@ethersproject/units';
 import { subgraphRequest } from '../../utils';
+import { multicall } from '../../utils';
 
 const KEEP_SUBGRAPH_URL = {
   '1': 'https://api.thegraph.com/subgraphs/name/miracle2k/all-the-keeps'
 };
+const TokenStaking = {
+  '1': "0x1293a54e160D1cd7075487898d65266081A15458"
+};
 
 export const author = 'corollari';
-export const version = '0.1.0';
+export const version = '0.1.1';
+
+const abi = [
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "balance",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
 
 export async function strategy(
   _space,
   network,
-  _provider,
+  provider,
   addresses,
   _options,
   snapshot
@@ -24,8 +53,8 @@ export async function strategy(
         },
         first: 1000
       },
-      owner: true,
-      stakedAmount: true
+      address: true,
+      owner: true
     }
   };
   if (snapshot !== 'latest') {
@@ -35,10 +64,23 @@ export async function strategy(
   const result = await subgraphRequest(KEEP_SUBGRAPH_URL[network], params);
   const score = {};
   if (result && result.operators) {
-    result.operators.forEach((op) => {
+    const balances = await multicall(
+      network,
+      provider,
+      abi,
+      result.operators.map(op => [
+        TokenStaking[network],
+        'balanceOf',
+        [op.address]
+      ]),
+      {
+        blockTag: typeof snapshot === 'number' ? snapshot : 'latest'
+      }
+    )
+    result.operators.forEach((op, i) => {
       const userAddress = getAddress(op.owner);
       if (!score[userAddress]) score[userAddress] = 0;
-      score[userAddress] = score[userAddress] + Number(op.stakedAmount);
+      score[userAddress] = score[userAddress] + Number(formatUnits(balances[i][0], 18));
     });
   }
   return score;
