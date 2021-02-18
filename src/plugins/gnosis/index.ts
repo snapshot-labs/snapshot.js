f86704927fe397b8680f875819c06429c11eacb0
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2021-02-18 19:20:40 +0100

    Add Uniswap inverted pairs query on Gnosis plugin (#101)
    * Add xDai support on Gnosis Impact plugin
    
    Add xDai thegraph Omen and HoneySwap API endpoints for support with
    prediction markets on xDai network.
    
    * Fix HoneySwap thegraph endpoint and `web3._network.chainId` network id
    
    * Add Uniswap inverted pairs query on Gnosis plugin
    
    - Add `pairsTokensInverted` query for token inverted pairs.
    - Fix `network` type from number to string.

--- src/plugins/gnosis/index.ts
+++ src/plugins/gnosis/index.ts
@@ -4,7 +4,7 @@ import { multicall, subgraphRequest } from '../../utils';
 const UNISWAP_V2_SUBGRAPH_URL = {
   '1': 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
   '4': 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2-rinkeby',
-  '100': 'https://api.thegraph.com/subgraphs/name/1Hive/uniswap-v2'
+  '100': 'https://api.thegraph.com/subgraphs/name/1hive/uniswap-v2'
 };
 
 const OMEN_SUBGRAPH_URL = {
@@ -16,7 +16,7 @@ const OMEN_SUBGRAPH_URL = {
 const WETH_ADDRESS = {
   '1': '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
   '4': '0xc778417e063141139fce010982780140aa0cd5ab',
-  '100': '0x6a023ccd1ff6f2045c3309768ead9e68f978f6e1'
+  '100': '0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1'
 };
 
 const OMEN_GQL_QUERY = {
@@ -44,6 +44,16 @@ const UNISWAP_V2_GQL_QUERY = {
       }
     },
     token0Price: true
+  },
+  pairsTokensInverted: {
+    __aliasFor: 'pairs',
+    __args: {
+      where: {
+        token0: true,
+        token1: true
+      }
+    },
+    token1Price: true
   },
   pairsTokens0: {
     __aliasFor: 'pairs',
@@ -465,7 +475,7 @@ const erc20Abi = [
  * @param method
  */
 const getTokenInfo = async (web3, tokenAddress) => {
-  return await multicall(web3.network.chainId.toString(), web3, erc20Abi, [
+  return await multicall(web3._network.chainId.toString(), web3, erc20Abi, [
     [tokenAddress, 'name'],
     [tokenAddress, 'symbol']
   ]);
@@ -492,7 +502,7 @@ export default class Plugin {
     }
   }
 
-  async getOmenCondition(network: number, conditionId: any) {
+  async getOmenCondition(network: string, conditionId: any) {
     try {
       const query = OMEN_GQL_QUERY;
       query.condition.__args.id = conditionId;
@@ -502,12 +512,16 @@ export default class Plugin {
     }
   }
 
-  async getUniswapPair(network: number, token0: any, token1: any) {
+  async getUniswapPair(network: string, token0: any, token1: any) {
     try {
       const query = UNISWAP_V2_GQL_QUERY;
       query.pairsTokens.__args.where = {
         token0: token0.toLowerCase(),
         token1: token1.toLowerCase()
+      };
+      query.pairsTokensInverted.__args.where = {
+        token0: token1.toLowerCase(),
+        token1: token0.toLowerCase()
       };
       query.pairsTokens0.__args.where = {
         token0: token0.toLowerCase(),
@@ -524,6 +538,10 @@ export default class Plugin {
 
       if (result.pairsTokens.length > 0) {
         return result.pairsTokens[0];
+      } else if (result.pairsTokensInverted.length > 0) {
+        return {
+          token0Price: result.pairsTokensInverted[0].token1Price
+        };
       } else if (
         result.pairsTokens0.length > 0 &&
         result.pairsTokens1.length > 0
