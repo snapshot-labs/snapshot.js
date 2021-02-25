7bdb65aeb99b4165c729ba862a7a3e91086bd738
bonustrack <fabien@bonustrack.co>
2021-02-25 23:21:28 +0700

    Quick testing

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,16 @@
 import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
-import commonjs from '@rollup/plugin-commonjs';
-import globals from 'rollup-plugin-node-globals';
-import builtins from 'rollup-plugin-node-builtins';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import filesize from 'rollup-plugin-filesize';
 import json from 'rollup-plugin-json';
 import pkg from './package.json';
 
-const name = 'snapshot';
 const input = 'src/index.ts';
 const external = [...Object.keys(pkg.dependencies || {})];
 
 export default [
   {
     input,
-    context: 'window',
-    output: [
-      { name, file: pkg.browser, format: 'umd' }
-    ],
-    plugins: [
-      json(),
-      builtins(),
-      typescript({ clean: true }),
-      nodeResolve({ preferBuiltins: true, browser: true }),
-      commonjs(),
-      globals(),
-      terser(),
-      filesize()
-    ]
-  },
-  {
-    input,
     external,
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      { file: pkg.main, format: 'cjs' }
     ],
     plugins: [
       json(),
