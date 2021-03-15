db9ae1910f96d50c94b4fdbba91f0b76b6516535
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-03-15 11:40:47 +0900

    Update Pancake strategy
    Fetch user balance from our gouvernance API.

--- src/strategies/pancake/index.ts
+++ src/strategies/pancake/index.ts
@@ -1,65 +1,11 @@
-import { formatUnits } from '@ethersproject/units';
-import { multicall } from '../../utils';
-import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
-
 export const author = 'pancake-swap';
 export const version = '0.0.1';
 
-const sousChefabi = [
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: '',
-        type: 'address'
-      }
-    ],
-    name: 'userInfo',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: 'amount',
-        type: 'uint256'
-      }
-    ],
-    stateMutability: 'view',
-    type: 'function'
-  }
-];
+interface ApiReturn {
+  staked: number;
+}
 
-const masterChefAbi = [
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
-      },
-      {
-        internalType: 'address',
-        name: '',
-        type: 'address'
-      }
-    ],
-    name: 'userInfo',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: 'amount',
-        type: 'uint256'
-      },
-      {
-        internalType: 'uint256',
-        name: 'rewardDebt',
-        type: 'uint256'
-      }
-    ],
-    stateMutability: 'view',
-    type: 'function'
-  }
-];
-
-const masterChefContractAddress = '0x73feaa1eE314F8c655E354234017bE2193C9E24E';
+const apiUrl = 'https://gatsby-pancake-api-h3pi1aoet-pancakeswap.vercel.app';
 
 export async function strategy(
   space,
@@ -70,60 +16,18 @@ export async function strategy(
   snapshot
 ) {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
-  const score = await erc20BalanceOfStrategy(
-    space,
-    network,
-    provider,
-    addresses,
-    options,
-    snapshot
+
+  const promisesBalances = addresses.map((address) =>
+    fetch(
+      `${apiUrl}/api/governance/vote/${address}/${blockTag}`
+    ).then((response) => response.json())
+  );
+  const balances: ApiReturn[] = await Promise.all(promisesBalances);
+
+  const balancesMapping = addresses.reduce(
+    (acc, address, index) => ({ ...acc, [address]: balances[index].staked }),
+    {}
   );
 
-  const masterBalances = await multicall(
-    network,
-    provider,
-    masterChefAbi,
-    addresses.map((address: any) => [
-      masterChefContractAddress,
-      'userInfo',
-      ['0', address]
-    ]),
-    { blockTag }
-  );
-
-  const sousBalances = await Promise.all(
-    options.chefAddresses.map((item) =>
-      multicall(
-        network,
-        provider,
-        sousChefabi,
-        addresses.map((address: any) => [
-          item.address,
-          'userInfo',
-          [address],
-          { blockTag }
-        ]),
-        { blockTag }
-      )
-    )
-  );
-
-  return Object.fromEntries(
-    Object.entries(score).map((address, index) => [
-      address[0],
-      address[1] +
-        parseFloat(formatUnits(masterBalances[index].amount.toString(), 18)) +
-        sousBalances.reduce(
-          (prev: number, cur: any, idx: number) =>
-            prev +
-            parseFloat(
-              formatUnits(
-                cur[index].amount.toString(),
-                options.chefAddresses[idx].decimals
-              )
-            ),
-          0
-        )
-    ])
-  );
+  return balancesMapping;
 }
