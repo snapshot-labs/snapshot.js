58678673928c5d000a97b55f63f7f9ee5f9d37ed
bonustrack <fabien@bonustrack.co>
2021-03-20 04:36:23 +0700

    Map quorum plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,9 +1,11 @@
 import aragon from './aragon';
 import gnosis from './gnosis';
 import daoModule from './daoModule';
+import quorum from './quorum';
 
 export default {
   aragon,
   gnosis,
-  daoModule
+  daoModule,
+  quorum
 };
