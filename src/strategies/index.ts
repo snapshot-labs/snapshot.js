60d990c5bfadf0fc1b479e9f5462bebaf169ce84
noiach <gh@noiach.com>
2021-03-23 23:44:39 +1100

    Add ren-nodes strategy

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -63,6 +63,7 @@ import { strategy as pagination } from './pagination';
 import { strategy as rulerStakedLP } from './ruler-staked-lp';
 import { strategy as xcover } from './xcover';
 import { strategy as niuStaked } from './niu-staked';
+import { strategy as renNodes } from './ren-nodes';
 
 export default {
   balancer,
@@ -129,5 +130,6 @@ export default {
   pagination,
   'ruler-staked-lp': rulerStakedLP,
   xcover,
-  'niu-staked': niuStaked
+  'niu-staked': niuStaked,
+  'ren-nodes': renNodes
 };
