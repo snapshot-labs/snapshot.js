46a488000ae91904882f4c9b65f7b6546adfeec3
mushroomsforest <72915034+mushroomsforest@users.noreply.github.com>
2021-03-25 15:16:49 +0800

    fix variables and add symbol in example  (#140)
    * fix variables and add symbol in example
    
    * fix test for mushrooms
    
    * use formatUnits for lp
    
    * use current block instead of latest
    
    Co-authored-by: mandalorian-101 <mandalorian101@protonmail.com>
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -63,6 +63,7 @@ import { strategy as pagination } from './pagination';
 import { strategy as rulerStakedLP } from './ruler-staked-lp';
 import { strategy as xcover } from './xcover';
 import { strategy as niuStaked } from './niu-staked';
+import { strategy as mushrooms } from './mushrooms';
 import { strategy as curioCardsErc20Weighted } from './curio-cards-erc20-weighted';
 
 export default {
@@ -131,5 +132,6 @@ export default {
   'ruler-staked-lp': rulerStakedLP,
   xcover,
   'niu-staked': niuStaked,
+  'mushrooms': mushrooms,
   'curio-cards-erc20-weighted': curioCardsErc20Weighted
 };
