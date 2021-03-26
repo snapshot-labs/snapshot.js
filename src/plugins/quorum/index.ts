01def1ae2ded5def3e2126d91d340616ff88b73e
Leonid Beder <leonid.beder@gmail.com>
2021-03-27 01:00:26 +0300

    Improve support for numerical snapshot block numbers

--- src/plugins/quorum/index.ts
+++ src/plugins/quorum/index.ts
@@ -1,15 +1,20 @@
 import { BigNumber } from '@ethersproject/bignumber';
+import toNumber from 'lodash.tonumber';
 import { call } from '../../utils';
 
 export default class Plugin {
   public author = 'lbeder';
-  public version = '0.0.1';
+  public version = '0.1.0';
   public name = 'Quorum';
 
   /**
    * Returns the total voting power at specific snapshot
    */
-  async getTotalVotingPower(web3, quorumOptions, snapshot) {
+  async getTotalVotingPower(
+    web3: any,
+    quorumOptions: any,
+    snapshot: string | Number
+  ) {
     try {
       const { strategy } = quorumOptions;
 
@@ -21,7 +26,9 @@ export default class Plugin {
         case 'balance': {
           const { address, methodABI, decimals } = quorumOptions;
 
-          const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+          const blockTag =
+            snapshot === 'latest' ? snapshot : toNumber(snapshot);
+
           const totalVotingPower = await call(
             web3,
             [methodABI],
