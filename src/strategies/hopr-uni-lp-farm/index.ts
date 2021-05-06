36a8c838a59bca8f18dbfe6d213704e0aaea717d
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2021-05-06 12:48:13 +0200

    Include xHOPR and wxHOPR snapshot balance

--- src/strategies/hopr-uni-lp-farm/index.ts
+++ src/strategies/hopr-uni-lp-farm/index.ts
@@ -1,6 +1,7 @@
 import { formatUnits } from '@ethersproject/units';
 import { BigNumber } from '@ethersproject/bignumber';
-import { getScores, multicall } from '../../utils';
+import { getScores, multicall, subgraphRequest } from '../../utils';
+import { getBlockNumber } from '../../utils/web3';
 
 export const author = 'hoprnet';
 export const version = '0.1.0';
@@ -68,7 +69,50 @@ const tokenAndPoolAbi = [
     stateMutability:"view",
     type:"function"
  }
-]
+];
+
+const XDAI_BLOCK_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/1hive/xdai-blocks';
+const HOPR_XDAI_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/hoprnet/hopr-on-xdai';
+
+async function getXdaiBlockNumber (timestamp: number): Promise<number> {
+  const query = {
+    blocks: {
+      __args: {
+        first: 1,
+        orderBy: 'number',
+        orderDirection: 'desc',
+        where: {
+          timestamp_lte: timestamp
+        }
+      },
+      number: true,
+      timestamp: true
+    }
+  };
+  const data = await subgraphRequest(XDAI_BLOCK_SUBGRAPH_URL, query);
+  return Number(data.blocks[0].number);
+}
+
+async function xHoprSubgraphQuery (addresses: string[], blockNumber: number): Promise<{[propName: string]:number}> {
+  const query = {
+    accounts: {
+      __args: {
+        block: {
+          number: blockNumber
+        },
+        where: {
+          id_in: addresses.map(adr => adr.toLowerCase())
+        }
+      },
+      id: true,
+      totalBalance: true
+    }
+  }
+  const data = await subgraphRequest(HOPR_XDAI_SUBGRAPH_URL, query);
+  // map result (data.accounts) to addresses
+  const entries = data.accounts.map(d => [d.id, Number(d.totalBalance)])
+  return Object.fromEntries(entries);
+}
 
 export async function strategy(
   space,
@@ -104,6 +148,14 @@ export async function strategy(
     ? res.slice(2, 2 + addresses.length).map((r, i) => (r[0] as BigNumber).add(res[2 + i + addresses.length][1] as BigNumber))
     : res.slice(2).map(r => r[0] as BigNumber);
 
+  // get block timestamp to search on xDai subgraph
+  const snapshotTimestamp = (await provider.getBlock(blockTag)).timestamp;
+  const snapshotXdaiBlock = await getXdaiBlockNumber(snapshotTimestamp);
+  // get and parse xHOPR and wxHOPR balance
+  const hoprOnXdaiBalance = await xHoprSubgraphQuery(addresses, snapshotXdaiBlock);
+  const hoprOnXdaiScore = addresses.map(address => hoprOnXdaiBalance[address] ?? 0)
+
+  // get HOPR token balance
   const hoprScore = await getScores(space, [{
       name: "erc20-balance-of",
       params: {
@@ -120,6 +172,7 @@ export async function strategy(
         value.mul(hoprBalanceOfPool[0]).div(poolTotalSupply[0]
       ), 18))
       + hoprScore[0][addresses[i]] // HOPR token balance
+      + hoprOnXdaiScore[i] // xHOPR + wxHOPR balance
     ])
   );
 }
