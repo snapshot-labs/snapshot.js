02cdd7c9a41d38fc65082bc83dac8d95b85b78d9
Arpit Agarwal <93arpit@gmail.com>
2021-05-15 13:09:41 +0530

    [defidollar] update defidollar strategy (#217)
    * update defidollar strat
    
    * remove .div(1e18)
    
    * revert changes in package.lock
    
    * remove symbol and decimals

--- src/strategies/defidollar/index.ts
+++ src/strategies/defidollar/index.ts
@@ -1,9 +1,8 @@
 import { formatUnits } from '@ethersproject/units';
-import { BigNumber } from '@ethersproject/bignumber';
 import { multicall } from '../../utils';
 
 export const author = 'atvanguard';
-export const version = '0.1.0';
+export const version = '1.0.0';
 
 const abi = [
   {
@@ -23,78 +22,6 @@ const abi = [
         type: 'uint256'
       }
     ],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function'
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'account',
-        type: 'address'
-      }
-    ],
-    name: 'earned',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
-      }
-    ],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function'
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'account',
-        type: 'address'
-      }
-    ],
-    name: 'claimable_reward',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
-      }
-    ],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function'
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'account',
-        type: 'address'
-      }
-    ],
-    name: 'claimed_rewards_for',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
-      }
-    ],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function'
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: 'totalSupply',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     payable: false,
     stateMutability: 'view',
     type: 'function'
@@ -123,74 +50,20 @@ export async function strategy(
   const queries: any[] = [];
 
   addresses.forEach((voter) => {
-    queries.push([options.farms.curve.farm, 'claimable_reward', [voter]]);
+    queries.push([options.address, 'balanceOf', [voter]]);
   });
+  queries.push([options.address, 'getPricePerFullShare']);
 
-  addresses.forEach((voter) => {
-    queries.push([options.farms.curve.farm, 'claimed_rewards_for', [voter]]);
-  });
-
-  addresses.forEach((voter) => {
-    queries.push([options.farms.sushiEthDusd.farm, 'earned', [voter]]);
-  });
-
-  addresses.forEach((voter) => {
-    queries.push([options.farms.ibDFD.address, 'balanceOf', [voter]]);
-  });
-  queries.push([options.farms.ibDFD.address, 'getPricePerFullShare']);
-
-  const farms = ['balDfdDusd', 'sushiDfdEth'];
-  for (let i = 0; i < farms.length; i++) {
-    const { farm, lpToken } = options.farms[farms[i]];
-    queries.push([options.DFD, 'balanceOf', [lpToken]]);
-    queries.push([lpToken, 'totalSupply']);
-    addresses.forEach((voter) => {
-      queries.push([farm, 'balanceOf', [voter]]);
-      queries.push([lpToken, 'balanceOf', [voter]]);
-      queries.push([farm, 'earned', [voter]]);
-    });
-  }
-
-  let response = await multicall(network, provider, abi, queries, { blockTag });
-  response = response.map((r) => r[0]);
-  const n = addresses.length;
-
-  const dfdEarned = response.slice(0, n);
-  const dfdClaimed = response.slice(n, 2 * n);
-  const sushiEthDusdEarned = response.slice(2 * n, 3 * n);
-  const ppfs = response[4 * n];
-  const ibDFD = response
-    .slice(3 * n, 4 * n)
-    .map((r) => r.mul(ppfs).div(BigNumber.from(10).pow(18)));
-  response = response.slice(4 * n + 1);
+  const response = (await multicall(network, provider, abi, queries, { blockTag })).map(r => r[0]);
+  const pps = response[response.length - 1]
 
   return Object.fromEntries(
     Array(addresses.length)
       .fill('x')
       .map((_, i) => {
-        let score = dfdEarned[i]
-          .sub(dfdClaimed[i])
-          .add(sushiEthDusdEarned[i])
-          .add(ibDFD[i]);
-        while (response.length) {
-          const res = response.slice(0, 2 + 3 * n); // 2 + 3n queries for each farm
-          response = response.slice(2 + 3 * n);
-          /*
-            lpTokenBalance = farm.balanceOf(user) + lpToken.balanceOf(user)
-            staked = (dfd.balanceOf(lpToken) * lpTokenBalance) / lpToken.totalSupply()
-            earned = farm.earned(user)
-            score = staked + earned
-          */
-          const start = 2 + 3 * i;
-          const lpTokenBalance = res[start].add(res[start + 1]);
-          const staked = res[0].mul(lpTokenBalance).div(res[1]);
-          const earned = res[start + 2];
-          score = score.add(staked).add(earned);
-        }
-
         return [
           addresses[i],
-          parseFloat(formatUnits(score.toString(), 18 /* decimals */))
+          parseFloat(formatUnits(response[i].mul(pps), 36 /* decimals */))
         ];
       })
   );
