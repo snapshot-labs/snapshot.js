fc04196bd9128b899e4c61a0d38215df840fcb12
bonustrack <fabien@bonustrack.co>
2021-06-03 15:04:38 +0700

    Lint code

--- src/strategies/xrc20-balance-of/index.ts
+++ src/strategies/xrc20-balance-of/index.ts
@@ -12,7 +12,7 @@ const testNetUrl = 'https://testnet.iotexscout.io/apiproxy';
 const mainNetUrl = 'https://iotexscout.io/apiproxy';
 
 function getUrl(network) {
-  return (network == 4689) ? mainNetUrl : testNetUrl;
+  return network == 4689 ? mainNetUrl : testNetUrl;
 }
 
 export async function strategy(
@@ -25,27 +25,34 @@ export async function strategy(
 ) {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
   if (blockTag == 'latest')
-    return erc20BalanceStrategy(space, network, provider, addresses, options, snapshot);
+    return erc20BalanceStrategy(
+      space,
+      network,
+      provider,
+      addresses,
+      options,
+      snapshot
+    );
 
   const apiUrl = getUrl(network);
-  const response = await fetch(`${apiUrl}/api.AccountService.GetErc20TokenBalanceByHeight`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      address: addresses,
-      height: snapshot,
-      contract_address: options.address
-    })
-  });
+  const response = await fetch(
+    `${apiUrl}/api.AccountService.GetErc20TokenBalanceByHeight`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        address: addresses,
+        height: snapshot,
+        contract_address: options.address
+      })
+    }
+  );
 
   const ret: ApiReturn = await response.json();
   return Object.fromEntries(
-    ret.balance.map((v, i) => [
-      addresses[i],
-      parseFloat(v)
-    ])
+    ret.balance.map((v, i) => [addresses[i], parseFloat(v)])
   );
 }
