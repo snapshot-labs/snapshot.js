69a429d1b1380a44123bdcb097be57fbae9cf280
mas walker <handsome.void@gmail.com>
2021-06-03 16:03:58 +0800

    add xrc20-balance-of strategy for iotex (#248)
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -89,6 +89,7 @@ import { strategy as apescape } from './apescape';
 import { strategy as liftkitchen } from './liftkitchen';
 import { strategy as decentralandEstateSize } from './decentraland-estate-size';
 import { strategy as iotexBalance } from './iotex-balance';
+import { strategy as xrc20BalanceOf } from './xrc20-balance-of';
 import { strategy as brightid } from './brightid';
 import { strategy as inverseXINV } from './inverse-xinv';
 import { strategy as spookyswap } from './spookyswap';
@@ -187,5 +188,6 @@ export default {
   brightid,
   'inverse-xinv': inverseXINV,
   'iotex-balance': iotexBalance,
+  'xrc20-balance-of': xrc20BalanceOf,
   spookyswap
 };
