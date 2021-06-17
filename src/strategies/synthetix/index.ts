398b3b378db923efacf34c4ca27e616a31fd06bf
AndyTCF <andy.c.f.98@gmail.com>
2021-06-17 11:33:50 +1000

    initial implementation of ovm staking weight

--- src/strategies/synthetix/index.ts
+++ src/strategies/synthetix/index.ts
@@ -1,15 +1,67 @@
 import { formatUnits } from '@ethersproject/units';
-import { subgraphRequest } from '../../utils';
 import { getAddress } from '@ethersproject/address';
+import { subgraphRequest } from '../../utils';
+import { BigNumber } from '@ethersproject/bignumber';
 
 export const author = 'andytcf';
-export const version = '0.0.1';
+export const version = '1.0.0';
 
-const SYNTHETIX_SUBGRAPH_URL = `https://api.thegraph.com/subgraphs/name/synthetixio-team/synthetix`;
+type SNXHoldersResult = {
+  snxholders: {
+    id: string;
+    initialDebtOwnership: BigNumber;
+  }[];
+};
 
-const quadraticWeighting = (value) => {
+const defaultGraphs = {
+  '1': 'https://api.thegraph.com/subgraphs/name/synthetixio-team/synthetix',
+  '10':
+    'https://api.thegraph.com/subgraphs/name/synthetixio-team/optimism-general'
+};
+
+async function getChainBlockNumber(
+  timestamp: number,
+  graphURL: string
+): Promise<number> {
+  const query = {
+    blocks: {
+      __args: {
+        first: 1,
+        orderBy: 'number',
+        orderDirection: 'desc',
+        where: {
+          timestamp_lte: timestamp
+        }
+      },
+      number: true,
+      timestamp: true
+    }
+  };
+  const data = await subgraphRequest(graphURL, query);
+  return Number(data.blocks[0].number);
+}
+
+function returnGraphParams(blockNumber: number | string, addresses: string[]) {
+  return {
+    snxholders: {
+      __args: {
+        where: {
+          id_in: addresses.map((address: string) => address.toLowerCase())
+        },
+        first: 1000,
+        block: {
+          number: blockNumber
+        }
+      },
+      id: true,
+      initialDebtOwnership: true
+    }
+  };
+}
+
+const quadraticWeighting = (value: BigNumber) => {
   // Scale the value by 100000
-  const scaledValue = value * 1e5;
+  const scaledValue = parseFloat(formatUnits(value.toString(), 27)) * 1e5;
   return Math.sqrt(scaledValue);
 };
 
@@ -17,36 +69,49 @@ export async function strategy(
   _space,
   _network,
   _provider,
-  addresses,
+  _addresses,
   _,
   snapshot
 ) {
-  const params = {
-    snxholders: {
-      __args: {
-        where: {
-          id_in: addresses.map((address: string) => address.toLowerCase())
-        },
-        first: 1000
-      },
-      id: true,
-      initialDebtOwnership: true
-    }
-  };
-
-  if (snapshot !== 'latest') {
-    // @ts-ignore
-    params.snxholders.__args.block = { number: snapshot };
-  }
-
-  const result = await subgraphRequest(SYNTHETIX_SUBGRAPH_URL, params);
   const score = {};
 
-  if (result && result.snxholders) {
-    result.snxholders.forEach((holder) => {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+  const block = await _provider.getBlock(blockTag);
+
+  const ovmBlocknumber = await getChainBlockNumber(
+    block.timestamp,
+    defaultGraphs[10]
+  );
+
+  const l1Results = (await subgraphRequest(
+    defaultGraphs[1],
+    returnGraphParams(blockTag, _addresses)
+  )) as SNXHoldersResult;
+
+  const ovmResults = (await subgraphRequest(
+    defaultGraphs[10],
+    returnGraphParams(ovmBlocknumber, _addresses)
+  )) as SNXHoldersResult;
+
+  if (l1Results && l1Results.snxholders) {
+    l1Results.snxholders.forEach((holder) => {
       score[getAddress(holder.id)] = quadraticWeighting(
-        parseFloat(formatUnits(holder.initialDebtOwnership.toString(), 27))
+        holder.initialDebtOwnership
       );
+    });
+  }
+
+  if (ovmResults && ovmResults.snxholders) {
+    ovmResults.snxholders.forEach((holder) => {
+      if (score[getAddress(holder.id)] > 0) {
+        score[getAddress(holder.id)] += quadraticWeighting(
+          holder.initialDebtOwnership
+        );
+      } else {
+        score[getAddress(holder.id)] = quadraticWeighting(
+          holder.initialDebtOwnership
+        );
+      }
     });
   }
 
