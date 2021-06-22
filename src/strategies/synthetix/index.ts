6eb5b2afee6c0c556d76c4dc9ef098165ba4b99a
andytcf <39738607+andytcf@users.noreply.github.com>
2021-06-22 19:22:46 +1000

    [synthetix] Feat/extend synthetix strategy (#265)
    * add synthetix strategy
    
    * remove prettier quotes
    
    * revert back to single quotes
    
    * revert back to single quotes (2)
    
    * add moduleResolution
    
    * add a try catch into getScores for better debugging
    
    * add ilv strategy
    
    * add readme
    
    * refactor strategy
    
    * convert synthetix strategy to graph query
    
    * fix up subgraph query
    
    * add checksum
    
    * initial implementation of ovm staking weight
    
    * add fallback logic in case graph doesnt work
    
    * add snapshot history
    
    * refactor json to ipfs
    
    * add more than 500 addresses as example

--- src/strategies/synthetix/index.ts
+++ src/strategies/synthetix/index.ts
@@ -1,15 +1,47 @@
 import { formatUnits } from '@ethersproject/units';
-import { subgraphRequest } from '../../utils';
 import { getAddress } from '@ethersproject/address';
+import { subgraphRequest, ipfsGet } from '../../utils';
+import { BigNumber } from '@ethersproject/bignumber';
 
 export const author = 'andytcf';
-export const version = '0.0.1';
+export const version = '1.0.0';
 
-const SYNTHETIX_SUBGRAPH_URL = `https://api.thegraph.com/subgraphs/name/synthetixio-team/synthetix`;
+type SNXHoldersResult = {
+  snxholders: {
+    id: string;
+    initialDebtOwnership: BigNumber;
+  }[];
+};
 
-const quadraticWeighting = (value) => {
+const defaultGraphs = {
+  '1': 'https://api.thegraph.com/subgraphs/name/synthetixio-team/synthetix',
+  '10':
+    'https://api.thegraph.com/subgraphs/name/synthetixio-team/optimism-general'
+};
+
+const ovmSnapshotJSON = 'QmNwvhq4By1Mownjycg7bWSXqbJWMVyAWRZ1K4mjxuvGXg';
+
+function returnGraphParams(blockNumber: number | string, addresses: string[]) {
+  return {
+    snxholders: {
+      __args: {
+        where: {
+          id_in: addresses.map((address: string) => address.toLowerCase())
+        },
+        first: 1000,
+        block: {
+          number: blockNumber
+        }
+      },
+      id: true,
+      initialDebtOwnership: true
+    }
+  };
+}
+
+const quadraticWeighting = (value: BigNumber) => {
   // Scale the value by 100000
-  const scaledValue = value * 1e5;
+  const scaledValue = parseFloat(formatUnits(value.toString(), 27)) * 1e5;
   return Math.sqrt(scaledValue);
 };
 
@@ -17,38 +49,62 @@ export async function strategy(
   _space,
   _network,
   _provider,
-  addresses,
+  _addresses,
   _,
   snapshot
 ) {
-  const params = {
-    snxholders: {
-      __args: {
-        where: {
-          id_in: addresses.map((address: string) => address.toLowerCase())
-        },
-        first: 1000
-      },
-      id: true,
-      initialDebtOwnership: true
-    }
-  };
-
-  if (snapshot !== 'latest') {
-    // @ts-ignore
-    params.snxholders.__args.block = { number: snapshot };
-  }
-
-  const result = await subgraphRequest(SYNTHETIX_SUBGRAPH_URL, params);
   const score = {};
 
-  if (result && result.snxholders) {
-    result.snxholders.forEach((holder) => {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+
+  const l1Results = (await subgraphRequest(
+    defaultGraphs[1],
+    returnGraphParams(blockTag, _addresses)
+  )) as SNXHoldersResult;
+
+  // const ovmResults = (await subgraphRequest(
+  //   defaultGraphs[10],
+  //   returnGraphParams(53125, _addresses)
+  // )) as SNXHoldersResult;
+
+  if (l1Results && l1Results.snxholders) {
+    l1Results.snxholders.forEach((holder) => {
       score[getAddress(holder.id)] = quadraticWeighting(
-        parseFloat(formatUnits(holder.initialDebtOwnership.toString(), 27))
+        holder.initialDebtOwnership
       );
     });
   }
+
+  const OVMSnapshot = await ipfsGet('gateway.pinata.cloud', ovmSnapshotJSON);
+
+  const array = Object.assign(
+    {},
+    ...OVMSnapshot.data.snxholders.map((key) => ({
+      [getAddress(key.id)]: key.initialDebtOwnership
+    }))
+  );
+
+  _addresses.map((address) => {
+    if (array[getAddress(address)]) {
+      score[getAddress(address)] = quadraticWeighting(
+        array[getAddress(address)]
+      );
+    }
+  });
+
+  // if (ovmResults && ovmResults.snxholders) {
+  //   ovmResults.snxholders.forEach((holder) => {
+  //     if (score[getAddress(holder.id)] > 0) {
+  //       score[getAddress(holder.id)] += quadraticWeighting(
+  //         holder.initialDebtOwnership
+  //       );
+  //     } else {
+  //       score[getAddress(holder.id)] = quadraticWeighting(
+  //         holder.initialDebtOwnership
+  //       );
+  //     }
+  //   });
+  // }
 
   return score || {};
 }
