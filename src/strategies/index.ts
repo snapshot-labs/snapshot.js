151904d6e75174c4ef3930c74d5ad0cb2c2dc3e1
Ferit Tunçer <0xferittuncer@gmail.com>
2021-06-30 15:42:03 +0300

    refactor(strategies): add erc20-balance-of-quadratic-delegation

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -8,6 +8,8 @@ import { strategy as erc20BalanceOfFixedTotal } from './erc20-balance-of-fixed-t
 import { strategy as erc20BalanceOfCv } from './erc20-balance-of-cv';
 import { strategy as erc20WithBalance } from './erc20-with-balance';
 import { strategy as erc20BalanceOfDelegation } from './erc20-balance-of-delegation';
+import { strategy as erc20BalanceOfQuadraticDelegation } from './erc20-balance-of-quadratic-delegation';
+
 import { strategy as erc20Price } from './erc20-price';
 import { strategy as balanceOfWithMin } from './balance-of-with-min';
 import { strategy as ethBalance } from './eth-balance';
