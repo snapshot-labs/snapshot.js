e3f43dd3128f2c46bde8fd3b4cc733a340344580
ash-greninja-poke <85923042+ash-greninja-poke@users.noreply.github.com>
2021-07-02 21:50:58 +0530

    Update src/strategies/index.ts
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -209,5 +209,5 @@ export default {
   'infinityprotocol-liquidity-pools': infinityProtocolPools,
   'aave-governance-power': aaveGovernancePower,
   cake,
-  planetFinance
+  'planet-finance': planetFinance
 };
