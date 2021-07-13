8909910a405e8622c734af988bfbbda2122c126d
Kiba Gateaux <malikwormsby@gmail.com>
2021-07-13 04:15:13 -0500

    [balancer-smart-pool] Add/balancer smart pool token score (#303)
    * example balancer pool scoring
    
    * adds example for balancer smart pools
    
    * swap pool and token address in params
    
    * update graph params
    
    * adds balancer-smart-pool  to strategies list
    
    * update balancer smart pool strategy
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

---
+++ src/strategies/balancer-smart-pool/index.ts
@@ -0,0 +1,66 @@
+import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
+import { subgraphRequest } from '../../utils';
+
+const BALANCER_SUBGRAPH_URL = {
+  '1': 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer'
+};
+
+export const author = 'kibagateaux';
+export const version = '0.1.0';
+
+export async function strategy(
+  space,
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+) {
+  const score = await erc20BalanceOfStrategy(
+    space,
+    network,
+    provider,
+    addresses,
+    options,
+    snapshot
+  );
+  if(Object.keys(score).length === 0) return {}
+
+  const poolQueryParams = {
+    pools: {
+      __args: {
+        where: {
+          id: options.pool
+        }
+      },
+      tokens: {
+        __args: {
+          where: {
+            address: options.governanceToken
+          }
+        },
+        balance: true
+      }
+    }
+  };
+  if (snapshot !== 'latest') {
+    // @ts-ignore
+    poolQueryParams.pools.__args.block = { number: snapshot };
+  }
+
+  const result = await subgraphRequest(
+    BALANCER_SUBGRAPH_URL[network],
+    poolQueryParams
+  );
+  if(!result || !result.pools) return {}
+  
+  const totalScore = Object.values(score).reduce((a, b) => a + b, 0);
+  const poolTokenBalance = result.pools[0].tokens[0].balance;
+
+  return Object.fromEntries(
+    Object.entries(score).map((account) => [
+      account[0],
+      (account[1] / totalScore) * poolTokenBalance
+    ])
+  );
+}
