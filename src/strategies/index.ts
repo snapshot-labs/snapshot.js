8223d51305ae0847b06b1007cb38ae5bdc473165
dapplion <35266934+dapplion@users.noreply.github.com>
2021-07-17 08:44:55 +0200

    Add to strategies/index

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -113,6 +113,7 @@ import { strategy as planetFinance } from './planet-finance';
 import { strategy as impossibleFinance } from './impossible-finance';
 import { strategy as trancheStakingSLICE } from './tranche-staking-slice';
 import { strategy as unipoolSameToken } from './unipool-same-token';
+import { strategy as unipoolUniv2Lp } from './unipool-univ2-lp';
 
 export default {
   balancer,
@@ -229,5 +230,6 @@ export default {
   'impossible-finance': impossibleFinance,
   badgeth,
   'tranche-staking-slice': trancheStakingSLICE,
-  'unipool-same-token': unipoolSameToken
+  'unipool-same-token': unipoolSameToken,
+  'unipool-univ2-lp': unipoolUniv2Lp
 };
