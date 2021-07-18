ff5e4cd262a377ac2e65886ec2a552244f18e4a7
ash-greninja-poke <85923042+ash-greninja-poke@users.noreply.github.com>
2021-07-19 02:35:11 +0530

    [planet-finance] New Voting Strategy (#314)
    * push
    
    * change in strategy
    
    * Update src/strategies/index.ts
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>
    
    * change in strategy name
    
    * Update index.ts
    
    * Update examples.json
    
    * Update index.ts
    
    * new strategy
    
    * sync
    
    Co-authored-by: Namit Jain <nj@rapidinnovation.dev>
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/planet-finance/index.ts
+++ src/strategies/planet-finance/index.ts
@@ -29,38 +29,40 @@ const planetFinanceFarmAbi = [
     outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function'
-  }
+  },
 ];
 
 export const bep20Abi: any = [
-  {
-    constant: true,
-    inputs: [],
-    name: 'totalSupply',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function'
-  },
-  {
-    constant: true,
-    inputs: [{ internalType: 'address', name: '', type: 'address' }],
-    name: 'balanceOf',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function'
-  }
-];
+    {
+      constant: true,
+      inputs: [],
+      name: 'totalSupply',
+      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      constant: true,
+      inputs: [{ internalType: 'address', name: '', type: 'address' }],
+      name: 'balanceOf',
+      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ]
 
 const planetFinanceFarmContractAddress =
   '0x0ac58Fd25f334975b1B61732CF79564b6200A933';
 
-const aquaAddress = '0x72B7D61E8fC8cF971960DD9cfA59B8C829D91991';
+const aquaAddress = '0x72B7D61E8fC8cF971960DD9cfA59B8C829D91991'
 
-const aquaBnbLpTokenAddress = '0x03028D2F8B275695A1c6AFB69A4765e3666e36d9';
+const aquaBnbLpTokenAddress = '0x03028D2F8B275695A1c6AFB69A4765e3666e36d9'
 
-const aquaCakeLpTokenAddress = '0x8852263275Ab21FfBAEB88a17BCb27611EeA54Ef';
+const aquaCakeLpTokenAddress = '0x8852263275Ab21FfBAEB88a17BCb27611EeA54Ef'
+
+const aquaBusdLpTokenAddress = '0x0DcFde6c6761286AE0FF26abE65c30c8918889Ca'
 
 export async function strategy(
   space,
@@ -75,8 +77,8 @@ export async function strategy(
   const erc20Multi = new Multicaller(network, provider, bep20Abi, {
     blockTag
   });
-
-  const score = await erc20BalanceOfStrategy(
+  // returns user's aqua balance ofr their address
+  let score:any = erc20BalanceOfStrategy(
     space,
     network,
     provider,
@@ -85,7 +87,8 @@ export async function strategy(
     snapshot
   );
 
-  const usersAquaVaultBalances = await multicall(
+   // returns user's aqua balance in aqua only vault
+  let usersAquaVaultBalances:any =  multicall(
     network,
     provider,
     planetFinanceFarmAbi,
@@ -96,8 +99,9 @@ export async function strategy(
     ]),
     { blockTag }
   );
-
-  const usersAquaBnbVaultBalances = await multicall(
+  
+   // returns user's aqua balance in aqua-bnb vault
+  let usersAquaBnbVaultBalances:any =  multicall(
     network,
     provider,
     planetFinanceFarmAbi,
@@ -109,7 +113,8 @@ export async function strategy(
     { blockTag }
   );
 
-  const usersAquaCakeVaultBalances = await multicall(
+   // returns user's aqua balance in aqua-cake vault
+  let usersAquaCakeVaultBalances:any =  multicall(
     network,
     provider,
     planetFinanceFarmAbi,
@@ -121,45 +126,97 @@ export async function strategy(
     { blockTag }
   );
 
-  erc20Multi.call('lpTotalSupply', aquaBnbLpTokenAddress, 'totalSupply');
+   // returns user's aqua balance in aqua-busd vault
+  let usersAquaBusdVaultBalances:any = multicall(
+    network,
+    provider,
+    planetFinanceFarmAbi,
+    addresses.map((address: any) => [
+      planetFinanceFarmContractAddress,
+      'stakedWantTokens',
+      ['36', address]
+    ]),
+    { blockTag }
+  );
 
-  erc20Multi.call('poolMMBalance', aquaAddress, 'balanceOf', [
+  let result = await Promise.all([score,
+    usersAquaVaultBalances,
+    usersAquaBnbVaultBalances,
+    usersAquaCakeVaultBalances,
+    usersAquaBusdVaultBalances])
+
+    score = result[0];
+    usersAquaVaultBalances = result[1];
+    usersAquaBnbVaultBalances = result[2];
+    usersAquaCakeVaultBalances = result[3];
+    usersAquaBusdVaultBalances = result[4];
+
+  
+
+  //AQUA-BNB
+  erc20Multi.call(
+    'aquaBnbTotalSupply',
+    aquaBnbLpTokenAddress,
+    'totalSupply'
+  );
+
+  erc20Multi.call('lpAquaBal', aquaAddress , 'balanceOf', [
     aquaBnbLpTokenAddress
   ]);
 
   let erc20Result = await erc20Multi.execute();
 
-  const totalSupply = erc20Result.lpTotalSupply.toString();
+  let totalSupply = erc20Result.aquaBnbTotalSupply.toString();
 
-  const contractAquaBalance = erc20Result.poolMMBalance.toString();
+  let contractAquaBalance = erc20Result.lpAquaBal.toString()
 
-  erc20Multi.call('lpTotalSupply', aquaCakeLpTokenAddress, 'totalSupply');
+  erc20Multi.call(
+    'lpTotalSupply',
+    aquaCakeLpTokenAddress,
+    'totalSupply'
+  );
 
-  erc20Multi.call('poolMMBalance', aquaAddress, 'balanceOf', [
+  erc20Multi.call('poolMMBalance', aquaAddress , 'balanceOf', [
     aquaCakeLpTokenAddress
   ]);
 
   erc20Result = await erc20Multi.execute();
 
-  const totalSupplyAquaCake = erc20Result.lpTotalSupply.toString();
+  let totalSupplyAquaCake = erc20Result.lpTotalSupply.toString();
 
-  const aquaCakeContractAquaBalance = erc20Result.poolMMBalance.toString();
+  let aquaCakeContractAquaBalance = erc20Result.poolMMBalance.toString()
+
+
+
+  erc20Multi.call(
+    'lpTotalSupply',
+    aquaBusdLpTokenAddress,
+    'totalSupply'
+  );
+  //AQUA-BUSD
+  erc20Multi.call('poolMMBalance', aquaAddress , 'balanceOf', [
+    aquaBusdLpTokenAddress
+  ]);
+
+  erc20Result = await erc20Multi.execute();
+
+  let totalSupplyAquaBusd = erc20Result.lpTotalSupply.toString();
+
+  let aquaBusdContractAquaBalance = erc20Result.poolMMBalance.toString()
+
+  
 
   return Object.fromEntries(
-    Object.entries(score).map((address, index) => [
+    Object.entries(score).map((address:any, index) => [
       address[0],
       address[1] +
-        parseFloat(formatUnits(usersAquaVaultBalances[index].toString(), 18)) +
-        (parseFloat(
-          formatUnits(usersAquaBnbVaultBalances[index].toString(), 18)
-        ) /
-          parseFloat(formatUnits(totalSupply, 18))) *
-          parseFloat(formatUnits(contractAquaBalance, 18)) +
-        (parseFloat(
-          formatUnits(usersAquaCakeVaultBalances[index].toString(), 18)
-        ) /
-          parseFloat(formatUnits(totalSupplyAquaCake, 18))) *
-          parseFloat(formatUnits(aquaCakeContractAquaBalance, 18))
+      parseFloat(formatUnits(usersAquaVaultBalances[index].toString(),18))+
+      ((parseFloat(formatUnits(usersAquaBnbVaultBalances[index].toString(), 18)) / parseFloat(formatUnits(totalSupply,18)))
+      *(parseFloat(formatUnits(contractAquaBalance,18))))+
+      ((parseFloat(formatUnits(usersAquaCakeVaultBalances[index].toString(), 18)) / parseFloat(formatUnits(totalSupplyAquaCake,18)))
+      *(parseFloat(formatUnits(aquaCakeContractAquaBalance,18))))+
+      ((parseFloat(formatUnits(usersAquaBusdVaultBalances[index].toString(), 18)) / parseFloat(formatUnits(totalSupplyAquaBusd,18)))
+      *(parseFloat(formatUnits(aquaBusdContractAquaBalance,18))))
     ])
   );
 }
