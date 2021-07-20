348efebe223c9cb1b1ca62ee33ab666001cb107e
bonustrack <fabien@bonustrack.co>
2021-07-21 01:57:00 +0700

    Lint code

--- src/strategies/masterchef-pool-balance-price/index.ts
+++ src/strategies/masterchef-pool-balance-price/index.ts
@@ -1,6 +1,6 @@
 import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
-import { BigNumber} from '@ethersproject/bignumber';
+import { BigNumber } from '@ethersproject/bignumber';
 import fetch from 'cross-fetch';
 
 export const author = 'joaomajesus';
@@ -98,7 +98,7 @@ let _options;
 const getUserInfoCalls = (addresses: any[]) => {
   const result: any[] = [];
 
-  for (let address of addresses) {
+  for (const address of addresses) {
     result.push([_options.chefAddress, 'userInfo', [_options.pid, address]]);
   }
 
@@ -115,11 +115,11 @@ const getTokenCalls = () => {
     result.push([_options.uniPairAddress, 'token1', []]);
     result.push([_options.uniPairAddress, 'decimals', []]);
 
-    if(_options.token0?.address != null){
+    if (_options.token0?.address != null) {
       result.push([_options.token0.address, 'decimals', []]);
     }
 
-    if(_options.token1?.address != null){
+    if (_options.token1?.address != null) {
       result.push([_options.token1.address, 'decimals', []]);
     }
   }
@@ -142,37 +142,46 @@ async function processValues(
   tokenValues: any[],
   network: any,
   provider: any,
-  blockTag: string | number) {
-
+  blockTag: string | number
+) {
   log.push(`values = ${JSON.stringify(values, undefined, 2)}`);
   log.push(`tokenValues = ${JSON.stringify(tokenValues, undefined, 2)}`);
   printLog();
 
   const poolStaked = values[0][0] as BigNumber;
   const weight = BigNumber.from(_options.weight || 1);
-  const weightDecimals = BigNumber.from(10).pow(BigNumber.from(_options.weightDecimals || 0));
+  const weightDecimals = BigNumber.from(10).pow(
+    BigNumber.from(_options.weightDecimals || 0)
+  );
 
-  let result: number = 0;
+  let result = 0;
 
   if (_options.uniPairAddress == null) {
-      log.push(`poolStaked = ${poolStaked}`);
-      printLog();
+    log.push(`poolStaked = ${poolStaked}`);
+    printLog();
 
-      result = toFloat(poolStaked, _options.decimals);
+    result = toFloat(poolStaked, _options.decimals);
   } else {
     const uniTotalSupply = tokenValues[0][0];
     const uniReserve0 = tokenValues[1][0];
     const uniReserve1 = tokenValues[1][1];
-    const uniPairDecimalsIndex: any = _options.uniPairAddress != null ? 4 : null;
+    const uniPairDecimalsIndex: any =
+      _options.uniPairAddress != null ? 4 : null;
     const uniPairDecimalsCount = tokenValues[uniPairDecimalsIndex][0];
-    const uniPairDecimals = uniPairDecimalsIndex != null ? BigNumber.from(10).pow(BigNumber.from(uniPairDecimalsCount || 0)) : BigNumber.from(1);
+    const uniPairDecimals =
+      uniPairDecimalsIndex != null
+        ? BigNumber.from(10).pow(BigNumber.from(uniPairDecimalsCount || 0))
+        : BigNumber.from(1);
 
     const token0Address = tokenValues[2][0];
-    const useToken0 = _options.token0?.address != null && _options.token0.address.toString().toLowerCase() == token0Address?.toString().toLowerCase();
+    const useToken0 =
+      _options.token0?.address != null &&
+      _options.token0.address.toString().toLowerCase() ==
+        token0Address?.toString().toLowerCase();
 
     log.push(`useToken0 = ${useToken0}`);
 
-    if(useToken0){
+    if (useToken0) {
       const token0DecimalsIndex = 5;
 
       log.push(`token0DecimalsIndex = ${token0DecimalsIndex}`);
@@ -191,22 +200,26 @@ async function processValues(
         token0Address,
         token0DecimalsIndex,
         _options.token0?.weight,
-        _options.token0?.weightDecimals);
+        _options.token0?.weightDecimals
+      );
     }
 
     const token1Address = tokenValues[3][0];
-    const useToken1 = _options.token1?.address != null && _options.token1.address.toString().toLowerCase() == token1Address?.toString().toLowerCase();
+    const useToken1 =
+      _options.token1?.address != null &&
+      _options.token1.address.toString().toLowerCase() ==
+        token1Address?.toString().toLowerCase();
 
     log.push(`useToken1 = ${useToken1}`);
 
-    if(useToken1){
+    if (useToken1) {
       const token1DecimalsIndex = _options.token0?.address != null ? 6 : 5;
 
       log.push(`token1DecimalsIndex = ${token1DecimalsIndex}`);
       log.push(`tokenValues = ${JSON.stringify(tokenValues, undefined, 2)}`);
       printLog();
 
-      result += (await GetTokenValue(
+      result += await GetTokenValue(
         network,
         provider,
         blockTag,
@@ -218,26 +231,27 @@ async function processValues(
         token1Address,
         token1DecimalsIndex,
         _options.token1?.weight,
-        _options.token1?.WeightDecimals));
+        _options.token1?.WeightDecimals
+      );
     }
 
-    if(!useToken0 && !useToken1){
+    if (!useToken0 && !useToken1) {
       log.push(`poolStaked = ${poolStaked}`);
       log.push(`uniPairDecimals = ${uniPairDecimals}`);
       printLog();
 
-      const tokenCount = poolStaked.toNumber() / (10**uniPairDecimalsCount);
+      const tokenCount = poolStaked.toNumber() / 10 ** uniPairDecimalsCount;
 
       log.push(`tokenCount = ${tokenCount}`);
 
-      result = tokenCount / (10**(_options.decimals || 0));
+      result = tokenCount / 10 ** (_options.decimals || 0);
     }
   }
 
   log.push(`result = ${result}`);
   printLog();
 
-  result *= weight.toNumber()  / weightDecimals.toNumber();
+  result *= weight.toNumber() / weightDecimals.toNumber();
 
   log.push(`weight = ${weight}`);
   log.push(`weightDecimals = ${weightDecimals}`);
@@ -247,27 +261,31 @@ async function processValues(
   return applyAntiWhaleMeasures(result);
 }
 
-function applyAntiWhaleMeasures(result){
+function applyAntiWhaleMeasures(result) {
   log.push(`antiWhale = ${_options.antiWhale?.enable}`);
 
-  if(_options.antiWhale?.enable != true){
+  if (_options.antiWhale?.enable != true) {
     printLog();
     return result;
   }
 
-  const threshold = _options.antiWhale.threshold == null || _options.antiWhale.threshold <= 0
-    ? 1625
-    : _options.antiWhale.threshold;
+  const threshold =
+    _options.antiWhale.threshold == null || _options.antiWhale.threshold <= 0
+      ? 1625
+      : _options.antiWhale.threshold;
 
-  let inflectionPoint = _options.antiWhale.inflectionPoint == null || _options.antiWhale.inflectionPoint <= 0
-    ? 6500
-    : _options.antiWhale.inflectionPoint;
+  let inflectionPoint =
+    _options.antiWhale.inflectionPoint == null ||
+    _options.antiWhale.inflectionPoint <= 0
+      ? 6500
+      : _options.antiWhale.inflectionPoint;
 
   inflectionPoint = inflectionPoint < threshold ? threshold : inflectionPoint;
 
-  const exponent = _options.antiWhale.exponent == null || _options.antiWhale.exponent <= 0
-    ? 0.5
-    : _options.antiWhale.exponent > 1
+  const exponent =
+    _options.antiWhale.exponent == null || _options.antiWhale.exponent <= 0
+      ? 0.5
+      : _options.antiWhale.exponent > 1
       ? 1
       : _options.antiWhale.exponent;
 
@@ -276,10 +294,11 @@ function applyAntiWhaleMeasures(result){
   log.push(`threshold = ${threshold}`);
   printLog();
 
-  if (result > threshold){
-    result = inflectionPoint * ( result / inflectionPoint ) ** exponent;
+  if (result > threshold) {
+    result = inflectionPoint * (result / inflectionPoint) ** exponent;
   } else {
-    const thresholdMultiplier = ( inflectionPoint * ( threshold / inflectionPoint ) ** exponent ) / threshold;
+    const thresholdMultiplier =
+      (inflectionPoint * (threshold / inflectionPoint) ** exponent) / threshold;
 
     log.push(`thresholdMultiplier = ${thresholdMultiplier}`);
 
@@ -308,54 +327,71 @@ async function GetTokenValue(
   tokenAddress: any,
   tokenDecimalsIndex: any,
   tokenWeight: any,
-  tokenWeightDecimals: any) {
-    const weightDecimals = BigNumber.from(10).pow(BigNumber.from(tokenWeightDecimals || 0));
-    const weight = BigNumber.from(tokenWeight || 1);
-    const tokensPerLp = uniReserve.mul(uniPairDecimals).div(uniTotalSupply);
+  tokenWeightDecimals: any
+) {
+  const weightDecimals = BigNumber.from(10).pow(
+    BigNumber.from(tokenWeightDecimals || 0)
+  );
+  const weight = BigNumber.from(tokenWeight || 1);
+  const tokensPerLp = uniReserve.mul(uniPairDecimals).div(uniTotalSupply);
 
-    const tokenDecimals = tokenDecimalsIndex != null ? BigNumber.from(10).pow(BigNumber.from(tokenValues[tokenDecimalsIndex][0] || 0)) : BigNumber.from(1);
-    const price = await getTokenPrice(tokenAddress, network, provider, blockTag);
+  const tokenDecimals =
+    tokenDecimalsIndex != null
+      ? BigNumber.from(10).pow(
+          BigNumber.from(tokenValues[tokenDecimalsIndex][0] || 0)
+        )
+      : BigNumber.from(1);
+  const price = await getTokenPrice(tokenAddress, network, provider, blockTag);
 
-    log.push(`tokenAddress = ${tokenAddress}`);
-    log.push(`tokenDecimals = ${tokenDecimals}`);
-    log.push(`poolStaked = ${poolStaked}`);
-    log.push(`uniReserve = ${uniReserve}`);
-    log.push(`uniPairDecimals = ${uniPairDecimals}`);
-    log.push(`uniTotalSupply = ${uniTotalSupply}`);
-    log.push(`tokensPerLp = ${tokensPerLp}`);
-    log.push(`tokenWeight = ${weight}`);
-    log.push(`tokenWeightDecimals = ${weightDecimals}`);
-    log.push(`price = ${price}`);
+  log.push(`tokenAddress = ${tokenAddress}`);
+  log.push(`tokenDecimals = ${tokenDecimals}`);
+  log.push(`poolStaked = ${poolStaked}`);
+  log.push(`uniReserve = ${uniReserve}`);
+  log.push(`uniPairDecimals = ${uniPairDecimals}`);
+  log.push(`uniTotalSupply = ${uniTotalSupply}`);
+  log.push(`tokensPerLp = ${tokensPerLp}`);
+  log.push(`tokenWeight = ${weight}`);
+  log.push(`tokenWeightDecimals = ${weightDecimals}`);
+  log.push(`price = ${price}`);
 
-    printLog();
+  printLog();
 
-    const tokenCount = poolStaked
-      .mul(tokensPerLp)
-      .div(tokenDecimals)
-      .mul(weight)
-      .div(weightDecimals);
+  const tokenCount = poolStaked
+    .mul(tokensPerLp)
+    .div(tokenDecimals)
+    .mul(weight)
+    .div(weightDecimals);
 
-    log.push(`tokenCount = ${tokenCount}`);
+  log.push(`tokenCount = ${tokenCount}`);
 
-    return toFloat(tokenCount, _options.decimals) * price;
+  return toFloat(tokenCount, _options.decimals) * price;
 }
 
-function printLog(){
-  if(_options.log || false)
-  {
+function printLog() {
+  if (_options.log || false) {
     console.debug(log);
-    log = []
+    log = [];
   }
 }
 
-async function getTokenPrice(tokenAddress: any, network: any, provider: any, blockTag: string | number) {
-  let price: number = 1;
+async function getTokenPrice(
+  tokenAddress: any,
+  network: any,
+  provider: any,
+  blockTag: string | number
+) {
+  let price = 1;
   const cacheKey = tokenAddress + blockTag;
 
   if (_options.usePrice === true && !priceCache.has(cacheKey)) {
-     log.push(`calling getPrice for token address: ${tokenAddress} and blockTag: ${blockTag}`);
+    log.push(
+      `calling getPrice for token address: ${tokenAddress} and blockTag: ${blockTag}`
+    );
 
-     priceCache.set(cacheKey, await getPrice(network, provider, tokenAddress, blockTag) || 1);
+    priceCache.set(
+      cacheKey,
+      (await getPrice(network, provider, tokenAddress, blockTag)) || 1
+    );
   }
 
   price = priceCache.get(cacheKey) || 1;
@@ -363,21 +399,19 @@ async function getTokenPrice(tokenAddress: any, network: any, provider: any, blo
   return price;
 }
 
-async function getPrice(
-  network,
-  provider,
-  address,
-  blockTag) {
-  if(!blockCache.has(blockTag)){
+async function getPrice(network, provider, address, blockTag) {
+  if (!blockCache.has(blockTag)) {
     blockCache.set(blockTag, await provider.getBlock(blockTag));
   }
 
   const block = blockCache.get(blockTag);
   const platform = networksWithPlatforms[network];
-  const currency = _options.currency || 'usd'
+  const currency = _options.currency || 'usd';
   const from = block.timestamp - 100000;
   const to = block.timestamp;
-  const coingeckoApiURL = `https://api.coingecko.com/api/v3/coins/${platform}/contract/${new String(address).toLowerCase()}/market_chart/range?vs_currency=${currency}&from=${from}&to=${to}`;
+  const coingeckoApiURL = `https://api.coingecko.com/api/v3/coins/${platform}/contract/${new String(
+    address
+  ).toLowerCase()}/market_chart/range?vs_currency=${currency}&from=${from}&to=${to}`;
 
   log.push(`platform = ${platform}`);
   log.push(`from = ${from}`);
@@ -396,7 +430,9 @@ async function getPrice(
     })
     .catch((e) => {
       console.error(e);
-      throw new Error('Strategy masterchef-pool-balance-of-token: coingecko api failed');
+      throw new Error(
+        'Strategy masterchef-pool-balance-of-token: coingecko api failed'
+      );
     });
 
   return (coingeckoData.prices?.pop()?.pop() || 0) as number;
@@ -414,8 +450,7 @@ export async function strategy(
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
   const userInfoCalls = getUserInfoCalls(addresses);
   const tokenCalls = getTokenCalls();
-  const entries = new Map<PropertyKey,any>();
-
+  const entries = new Map<PropertyKey, any>();
 
   const userInfoResponse = await multicall(
     network,
@@ -425,22 +460,21 @@ export async function strategy(
     { blockTag }
   );
 
-  const userInfoChunks = arrayChunk(
-    userInfoResponse,
-    1
-  );
+  const userInfoChunks = arrayChunk(userInfoResponse, 1);
 
-  const tokenResponse = await multicall(
-    network,
-    provider,
-    abi,
-    tokenCalls,
-    { blockTag }
-  );
+  const tokenResponse = await multicall(network, provider, abi, tokenCalls, {
+    blockTag
+  });
 
   for (let i = 0; i < userInfoChunks.length; i++) {
     const value = userInfoChunks[i];
-    const score = await processValues(value, tokenResponse, network, provider, blockTag)
+    const score = await processValues(
+      value,
+      tokenResponse,
+      network,
+      provider,
+      blockTag
+    );
 
     entries.set(addresses[i], score);
   }
