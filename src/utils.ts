45d4bb85cd5ca0f88cee5dec431f5d72f1fdea1a
Sam <samuv.eth@gmail.com>
2021-07-21 09:57:31 +0700

    Add optional gateway param to getUrl

--- src/utils.ts
+++ src/utils.ts
@@ -84,18 +84,17 @@ export async function subgraphRequest(url: string, query, options: any = {}) {
   return data || {};
 }
 
-export function getUrl(uri) {
-  const ipfsGateway = `https://${gateways[0]}`;
+export function getUrl(uri, gateway = gateways[0]) {
+  const ipfsGateway = `https://${gateway}`;
   if (!uri) return null;
-
-  if (!uri.includes('ipfs') && !uri.includes('ipns') && !uri.includes('http')) {
+  if (!uri.includes('ipfs') && !uri.includes('ipns') && !uri.includes('http'))
     return `${ipfsGateway}/ipfs/${uri}`;
-  }
   const uriScheme = uri.split('://')[0];
   if (uriScheme === 'ipfs')
     return uri.replace('ipfs://', `${ipfsGateway}/ipfs/`);
   if (uriScheme === 'ipns')
     return uri.replace('ipns://', `${ipfsGateway}/ipns/`);
+
   return uri;
 }
 
