daf16d560c0cb3dd7ebc629bbd68370a49a35d6c
Sam <51686767+samuveth@users.noreply.github.com>
2021-07-21 15:39:35 +0700

    Changes for _ipfsUrl refactor (#324)
    * Changes for _ipfsUrl refactor
    
    * Fixes
    
    * Remove github url
    
    * Fixes
    
    * Add optional gateway param to getUrl
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/utils.ts
+++ src/utils.ts
@@ -84,13 +84,17 @@ export async function subgraphRequest(url: string, query, options: any = {}) {
   return data || {};
 }
 
-export function getUrl(uri) {
+export function getUrl(uri, gateway = gateways[0]) {
+  const ipfsGateway = `https://${gateway}`;
+  if (!uri) return null;
+  if (!uri.includes('ipfs') && !uri.includes('ipns') && !uri.includes('http'))
+    return `${ipfsGateway}/ipfs/${uri}`;
   const uriScheme = uri.split('://')[0];
-  const ipfsGateway = `https://${gateways[0]}`;
   if (uriScheme === 'ipfs')
     return uri.replace('ipfs://', `${ipfsGateway}/ipfs/`);
   if (uriScheme === 'ipns')
     return uri.replace('ipns://', `${ipfsGateway}/ipns/`);
+
   return uri;
 }
 
