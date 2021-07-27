3a5035e614f90293604c8dee95e991e2cd3f4d8e
Sam <samuv.eth@gmail.com>
2021-07-27 21:18:00 +0700

    Use env ipfs gateway if available

--- src/utils.ts
+++ src/utils.ts
@@ -84,7 +84,9 @@ export async function subgraphRequest(url: string, query, options: any = {}) {
   return data || {};
 }
 
-export function getUrl(uri, gateway = gateways[0]) {
+const defaultGateway = process.env.VUE_APP_IPFS_GATEWAY || gateways[0];
+
+export function getUrl(uri, gateway = defaultGateway) {
   const ipfsGateway = `https://${gateway}`;
   if (!uri) return null;
   if (!uri.includes('ipfs') && !uri.includes('ipns') && !uri.includes('http'))
