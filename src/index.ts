2ce4479e3bf28b339cfd053a2adf9ee616582653
Chaitanya <yourchaitu@gmail.com>
2021-07-27 22:31:04 +0530

    Move strategiesFull to original index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import Client from './client';
 import plugins from './plugins';
 import strategies from './strategies';
-import strategiesFull from './strategies/strategiesFull';
 import schemas from './schemas';
 import utils from './utils';
 
@@ -9,7 +8,6 @@ export default {
   Client,
   plugins,
   strategies,
-  strategiesFull,
   schemas,
   utils
 };
