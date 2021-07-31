aad05c27228f5ec00a8f7ac039ff52cde6998647
Chaitanya <yourchaitu@gmail.com>
2021-07-31 14:52:34 +0530

    provider fix for all eth network issues

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -3,8 +3,8 @@ import networks from '../networks.json';
 
 const providers = {};
 
-export default function getProvider(network: string) {
-  const url: string = networks[network].rpc[0];
+export default function getProvider(network: string, rpcIndex = 0) {
+  const url: string = networks[network].rpc[rpcIndex];
   if (!providers[network]) providers[network] = new StaticJsonRpcProvider(url);
   return providers[network];
 }
