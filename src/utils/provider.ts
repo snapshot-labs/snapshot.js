4b024f8be1911a3c42efaccb91640bf32b31838f
Chaitanya <yourchaitu@gmail.com>
2021-07-31 18:03:54 +0530

    fixes

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -3,8 +3,8 @@ import networks from '../networks.json';
 
 const providers = {};
 
-export default function getProvider(network: string, rpcIndex = 0) {
-  const url: string = networks[network].rpc[rpcIndex];
+export default function getProvider(network: string) {
+  const url: string = networks[network].rpc[0];
   if (!providers[network]) providers[network] = new StaticJsonRpcProvider(url);
   return providers[network];
 }
