5e1b91c7eac97c9f06fe5613014c9e8d2c0c6f7d
dapiguabc <dapiguabc@163.com>
2021-09-03 15:45:19 +0800

    :sparkler:  New plugin named Share.
    A plugin for social share.

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -3,11 +3,13 @@ import gnosis from './gnosis';
 import safeSnap from './safeSnap';
 import quorum from './quorum';
 import poap from './poap';
+import share from './share';
 
 export default {
   aragon,
   gnosis,
   safeSnap,
   poap,
-  quorum
+  quorum,
+  share
 };
---
+++ src/plugins/share/index.ts
@@ -0,0 +1,6 @@
+export default class Plugin {
+  public author = 'Kapp';
+  public version = '1.0.0';
+  public name = 'Share';
+  public options: any;
+}
