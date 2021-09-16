87aced4c74098385e9022e94b4944335fc0df45e
Fabien <bonustrack@users.noreply.github.com>
2021-09-16 20:27:22 +0700

    Remove plugins (#375)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import Client from './client';
-import plugins from './plugins';
 import schemas from './schemas';
 import utils from './utils';
 
 export default {
   Client,
-  plugins,
   schemas,
   utils
 };
--- src/plugins/charts/index.ts
+++
@@ -1,5 +0,0 @@
-export default class Plugin {
-  public author = 'zerquix18';
-  public version = '0.1.0';
-  public name = 'Charts';
-}
