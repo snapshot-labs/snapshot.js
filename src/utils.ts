4b5a588ec77f3e346e407411cfee3a6f08193c27
midgerate <midgerate@gmail.com>
2022-03-10 08:58:24 +0530

    fix(): use url instead of a new uri validator

--- src/utils.ts
+++ src/utils.ts
@@ -176,14 +176,6 @@ export function validateSchema(schema, data) {
   const ajv = new Ajv({ allErrors: true, allowUnionTypes: true, $data: true });
   // @ts-ignore
   addFormats(ajv);
-  ajv.addFormat('uri-strict', {
-    type: 'string',
-    validate: (str) => {
-      return /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(
-        str
-      );
-    }
-  });
   const validate = ajv.compile(schema);
   const valid = validate(data);
   return valid ? valid : validate.errors;
