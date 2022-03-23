6aae414be2298fe9db4c9079238a6bb642c7b330
Chaitanya <yourchaitu@gmail.com>
2022-03-23 13:00:37 +0530

    getBatchedProvider

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -16,8 +16,11 @@ export default function getProvider(network: string, type = 'archive') {
   return providers[network];
 }
 
-export function getBatchedProvider(network: string) {
-  const url: string = networks[network].rpc[0];
+export function getBatchedProvider(network: string, type = 'archive') {
+  let url: any = networks[network].rpc[0];
+  if(type === 'non-archive' && networks[network].nonArchiveRPC?.length) {
+    url =  networks[network].nonArchiveRPC[0];
+  }
   if (!providers[network]) providers[network] = new JsonRpcBatchProvider(url);
   return providers[network];
 }
