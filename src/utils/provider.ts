27ce110f68a6ef3e6c1093ecc0533947d2b7babb
Chaitanya <yourchaitu@gmail.com>
2022-03-23 12:47:30 +0530

    Non-Archive Nodes

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -6,8 +6,11 @@ import networks from '../networks.json';
 
 const providers = {};
 
-export default function getProvider(network: string) {
-  const url: any = networks[network].rpc[0];
+export default function getProvider(network: string, type = 'archive') {
+  let url: any = networks[network].rpc[0];
+  if(type === 'non-archive' && networks[network].nonArchiveRPC?.length) {
+    url =  networks[network].nonArchiveRPC[0];
+  }
   const connectionInfo = typeof url === 'object' ? {...url, timeout: 25000} : {url, timeout: 25000};
   if (!providers[network]) providers[network] = new StaticJsonRpcProvider(connectionInfo);
   return providers[network];
