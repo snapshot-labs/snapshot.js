2f505659c2b0afa061e9d6447cb850794f5667e6
midgerate <91108642+midgerate@users.noreply.github.com>
2022-04-13 21:37:48 +0530

    feat(): added profile schema (#557)
    * feat(): added profile schema
    
    * fix(): increase length of avatar and use uri spec
    
    * Update test/examples/profile.json
    
    Co-authored-by: Sam <51686767+samuveth@users.noreply.github.com>
    
    Co-authored-by: Sam <51686767+samuveth@users.noreply.github.com>

--- test/schema.ts
+++ test/schema.ts
@@ -2,6 +2,7 @@ const snapshot = require('../');
 const space = require('./examples/space.json');
 const proposal = require('./examples/proposal.json');
 const vote = require('./examples/vote.json');
+const profile = require('./examples/profile.json');
 
 const isValidSpace = snapshot.utils.validateSchema(
   snapshot.schemas.space,
@@ -17,3 +18,9 @@ console.log('Proposal', isValidProposal);
 
 const isValidVote = snapshot.utils.validateSchema(snapshot.schemas.vote, vote);
 console.log('Vote', isValidVote);
+
+const isValidProfile = snapshot.utils.validateSchema(
+  snapshot.schemas.profile,
+  profile
+);
+console.log('Profile', isValidProfile);
