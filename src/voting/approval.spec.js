886fb63be1e31cf67eeaba720f9e417770e7506c
Sam <samuv.eth@gmail.com>
2022-06-21 19:05:02 +0700

    Add tests for approval voting

---
+++ src/voting/approval.spec.js
@@ -0,0 +1,191 @@
+import { test, expect } from 'vitest';
+import ApprovalVoting from './approval';
+import example from './examples/approval.json';
+
+const example2 = () => {
+  // Exmaple with multiple (3) stratgies
+  const proposal = {
+    choices: ['Article 1', 'Article 2', 'Article 3', 'Article 4']
+  };
+  const strategies = [
+    { name: 'ticket', network: 1, params: {} },
+    { name: 'ticket', network: 1, params: {} },
+    { name: 'ticket', network: 1, params: {} }
+  ];
+  const scores = [771, 513, 417, 375];
+  const scoresByStrategy = [
+    [257, 257, 257],
+    [171, 171, 171],
+    [139, 139, 139],
+    [125, 125, 125]
+  ];
+  const scoresTotal = 1218;
+  const votes = example.votes.map((vote) => ({
+    choice: vote.choice,
+    balance: 3,
+    scores: [1, 1, 1]
+  }));
+
+  return {
+    proposal,
+    strategies,
+    scores,
+    scoresByStrategy,
+    scoresTotal,
+    votes
+  };
+};
+
+const rndChoices = () => {
+  const rndNumber = () => {
+    return Math.random() < 0.9
+      ? 2
+      : Math.random() < 0.7
+      ? -1
+      : Math.random() < 0.5
+      ? 1000000
+      : Math.random() < 0.3
+      ? 0
+      : undefined;
+  };
+  return [
+    rndNumber(),
+    rndNumber(),
+    rndNumber(),
+    rndNumber(),
+    rndNumber(),
+    rndNumber(),
+    rndNumber()
+  ];
+};
+
+const votesWithInvalidChoices = () => {
+  const votes = [];
+  for (let i = 0; i < 100; i++) {
+    votes.push({
+      choice: rndChoices(),
+      balance: 1,
+      scores: [1]
+    });
+  }
+  return [...votes, ...example.votes];
+};
+
+const votesWithInvalidChoices2 = () => {
+  const votes = [];
+  for (let i = 0; i < 100; i++) {
+    votes.push({
+      choice: rndChoices(),
+      balance: 3,
+      scores: [1, 1, 1]
+    });
+  }
+  return [...votes, ...example2().votes];
+};
+
+const getScoresTests = [
+  [example.proposal, example.votes, example.strategies, example.scores],
+  [
+    example.proposal,
+    votesWithInvalidChoices(),
+    example.strategies,
+    example.scores
+  ],
+  [
+    example2().proposal,
+    example2().votes,
+    example2().strategies,
+    example2().scores
+  ],
+  [
+    example2().proposal,
+    votesWithInvalidChoices2(),
+    example2().strategies,
+    example2().scores
+  ]
+];
+
+test.each(getScoresTests)(
+  'getScores',
+  (proposal, votes, strategies, expected) => {
+    const approval = new ApprovalVoting(proposal, votes, strategies, [1]);
+    expect(approval.getScores()).toEqual(expected);
+  }
+);
+
+const getScoresByStrategyTests = [
+  [
+    example.proposal,
+    example.votes,
+    example.strategies,
+    example.scoresByStrategy
+  ],
+  [
+    example.proposal,
+    votesWithInvalidChoices(),
+    example.strategies,
+    example.scoresByStrategy
+  ],
+  [
+    example2().proposal,
+    example2().votes,
+    example2().strategies,
+    example2().scoresByStrategy
+  ],
+  [
+    example2().proposal,
+    votesWithInvalidChoices2(),
+    example2().strategies,
+    example2().scoresByStrategy
+  ]
+];
+
+test.each(getScoresByStrategyTests)(
+  'getScoresByStrategy',
+  (proposal, votes, strategies, expected) => {
+    const approval = new ApprovalVoting(proposal, votes, strategies, [1]);
+    expect(approval.getScoresByStrategy()).toEqual(expected);
+  }
+);
+
+const getScoresTotalTests = [
+  [example.proposal, example.votes, example.strategies, example.scoresTotal],
+  [
+    example.proposal,
+    votesWithInvalidChoices(),
+    example.strategies,
+    example.scoresTotal
+  ],
+  [
+    example2().proposal,
+    example2().votes,
+    example2().strategies,
+    example2().scoresTotal
+  ],
+  [
+    example2().proposal,
+    votesWithInvalidChoices2(),
+    example2().strategies,
+    example2().scoresTotal
+  ]
+];
+
+test.each(getScoresTotalTests)(
+  'getScoresTotal',
+  (proposal, votes, strategies, expected) => {
+    const approval = new ApprovalVoting(proposal, votes, strategies, [1]);
+    expect(approval.getScoresTotal()).toEqual(expected);
+  }
+);
+
+test('getChoiceString', () => {
+  const approval = new ApprovalVoting(
+    example.proposal,
+    example.votes,
+    example.strategies,
+    example.selectedChoice
+  );
+  expect(approval.getChoiceString()).toEqual(
+    example.proposal.choices[example.selectedChoice - 1]
+  );
+});
--- src/voting/approval.ts
+++ src/voting/approval.ts
@@ -1,8 +1,24 @@
+function filterVotesWithInvalidChoice(votes, choices) {
+  return votes.filter((vote) => {
+    return (
+      Array.isArray(vote.choice) &&
+      // If choice index is not in choices, return false
+      vote.choice.every((choice) => choices?.[choice - 1] !== undefined) &&
+      // If any choice is duplicated, return false
+      vote.choice.length === new Set(vote.choice).size
+    );
+  });
+}
+
 export default class ApprovalVoting {
-  public proposal;
-  public votes;
-  public strategies;
-  public selected;
+  proposal: { choices: string[] };
+  votes: { choice: number[]; balance: number; scores: number[] }[];
+  strategies: {
+    name: string;
+    network: string;
+    params: Record<string, unknown>;
+  }[];
+  selected: number[];
 
   constructor(proposal, votes, strategies, selected) {
     this.proposal = proposal;
@@ -11,26 +27,30 @@ export default class ApprovalVoting {
     this.selected = selected;
   }
 
+  getValidatedVotes() {
+    return filterVotesWithInvalidChoice(this.votes, this.proposal.choices);
+  }
+
   getScores() {
     return this.proposal.choices.map((choice, i) =>
-      this.votes
-        .filter((vote: any) => vote.choice.includes(i + 1))
-        .reduce((a, b: any) => a + b.balance, 0)
+      this.getValidatedVotes()
+        .filter((vote) => vote.choice.includes(i + 1))
+        .reduce((a, b) => a + b.balance, 0)
     );
   }
 
   getScoresByStrategy() {
     return this.proposal.choices.map((choice, i) =>
       this.strategies.map((strategy, sI) =>
-        this.votes
-          .filter((vote: any) => vote.choice.includes(i + 1))
-          .reduce((a, b: any) => a + b.scores[sI], 0)
+        this.getValidatedVotes()
+          .filter((vote) => vote.choice.includes(i + 1))
+          .reduce((a, b) => a + b.scores[sI], 0)
       )
     );
   }
 
   getScoresTotal() {
-    return this.votes.reduce((a, b: any) => a + b.balance, 0);
+    return this.getValidatedVotes().reduce((a, b) => a + b.balance, 0);
   }
 
   getChoiceString() {
