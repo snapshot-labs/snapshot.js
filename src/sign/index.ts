34a5a5ff6e8e3e3a0e44fe35bbf5e8bce4b091b3
Zuza Zuber <zuza@lewagon.org>
2022-10-31 17:23:04 +0100

    comment out console.log

--- src/sign/index.ts
+++ src/sign/index.ts
@@ -63,7 +63,7 @@ export default class Client {
     message.timestamp = parseInt((Date.now() / 1e3).toFixed());
     const data: any = { domain, types, message };
     const sig = await signer._signTypedData(domain, data.types, message);
-    console.log('Sign', { address, sig, data });
+    // console.log('Sign', { address, sig, data });
 
     return await this.send({ address, sig, data });
   }
