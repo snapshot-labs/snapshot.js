7bd8a9eeadd13bf06f52ace6cf7a6bd7ada81c83
Chaitanya <yourchaitu@gmail.com>
2023-04-01 23:47:50 +0530

    Update utils.ts

--- src/sign/utils.ts
+++ src/sign/utils.ts
@@ -14,9 +14,13 @@ export async function verify(address, sig, data, network = '1') {
   // console.log('Hash', hash);
   // console.log('Address', address);
 
-  const recoverAddress = verifyTypedData(domain, types, message, sig);
-  // console.log('Recover address', recoverAddress);
-  if (address === recoverAddress) return true;
+  try {
+    const recoverAddress = verifyTypedData(domain, types, message, sig);
+    // console.log('Recover address', recoverAddress);
+    if (address === recoverAddress) return true;
+  } catch (e) {
+    // console.log('Could not recoverAddress:' + e.message);
+  }
 
   // console.log('Check EIP1271 signature');
   return await verifyEIP1271(address, sig, hash, network);
