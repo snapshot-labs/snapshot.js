f2268b216d45cacdf943638370aab022be435a04
MasterJediLNR <95591152+MasterJediLNR@users.noreply.github.com>
2023-04-19 14:41:19 -0400

    Turn on GZip compression by default (#811)
    * Turn on GZip compression by default
    
    Fixes #810.
    
    * Fix typescript issue.
    
    * Update src/utils/provider.ts
    
    * Update src/utils/provider.ts
    
    ---------
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -10,7 +10,11 @@ export default function getProvider(network) {
   const url = `https://rpc.snapshot.org/${network}`;
   if (!providers[network])
     providers[network] = new StaticJsonRpcProvider(
-      { url, timeout: 25000 },
+      {
+        url,
+        timeout: 25000,
+        allowGzip: true
+      },
       Number(network)
     );
   return providers[network];
@@ -19,9 +23,11 @@ export default function getProvider(network) {
 export function getBatchedProvider(network) {
   const url = `https://rpc.snapshot.org/${network}`;
   if (!batchedProviders[network])
-    batchedProviders[network] = new JsonRpcBatchProvider({
-      url,
-      timeout: 25000
-    });
+    batchedProviders[network] = new JsonRpcBatchProvider(
+      {
+        url,
+        timeout: 25000,
+        allowGzip: true
+      });
   return batchedProviders[network];
 }
