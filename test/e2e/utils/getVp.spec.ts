0df78dafe80ec0f9714cec35fdca1d58bc1af89a
ChaituVR <yourchaitu@gmail.com>
2023-11-17 12:16:43 +0530

    remove logs

--- test/e2e/utils/getVp.spec.ts
+++ test/e2e/utils/getVp.spec.ts
@@ -4,27 +4,6 @@ import { getVp } from '../../../src/utils';
 describe('test getVp', () => {
   test('getVp should return a promise rejection on error from score-api', async () => {
     expect.assertions(1);
-    console.log(
-      await getVp(
-        '0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11',
-        '123434',
-        [
-          {
-            name: 'erc20-balance-of',
-            params: {
-              symbol: 'TEST',
-              address: '0xc23F41519D7DFaDf9eed53c00f08C06CD5cDde54',
-              network: '1',
-              decimals: 18
-            },
-            network: '1'
-          }
-        ],
-        7929876,
-        'test.eth',
-        false
-      )
-    );
     await expect(
       getVp(
         '0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11',
