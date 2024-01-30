ebae573bc6a57ee1e93174d5887325c8983a56e3
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-01-30 15:41:59 +0700

    fix: handle case where there are more than 1000 duplicates

--- src/utils/delegation.ts
+++ src/utils/delegation.ts
@@ -2,6 +2,7 @@ import { subgraphRequest } from '../utils';
 import delegationSubgraphs from '../delegationSubgraphs.json';
 
 export const SNAPSHOT_SUBGRAPH_URL = delegationSubgraphs;
+const PAGE_SIZE = 1000;
 
 type Delegation = {
   delegator: string;
@@ -22,40 +23,32 @@ export default async function getDelegatesBySpace(
       `Delegation subgraph not available for network ${network}`
     );
   }
+
+  let result: Delegation[] = [];
+  let skip = 0;
   const spaceIn = ['', space];
   if (space.includes('.eth')) spaceIn.push(space.replace('.eth', ''));
 
-  const PAGE_SIZE = 1000;
-  let result: Delegation[] = [];
-  const params: any = {
-    delegations: {
-      __args: {
-        where: {
-          space_in: spaceIn
-        },
-        first: PAGE_SIZE,
-        skip: 0,
-        orderBy: 'timestamp',
-        orderDirection: 'asc'
-      },
-      delegator: true,
-      space: true,
-      delegate: true,
-      timestamp: true
-    }
-  };
-  if (snapshot !== 'latest') {
-    params.delegations.__args.block = { number: snapshot };
-  }
-
   while (true) {
-    params.delegations.__args.where.timestamp_gte =
-      result[result.length - 1]?.timestamp || 0;
-
-    const newResults: Delegation[] =
-      (await subgraphRequest(subgraphUrl, params)).delegations || [];
+    const newResults = await fetchData({
+      url: subgraphUrl,
+      spaces: spaceIn,
+      first: PAGE_SIZE,
+      skip,
+      pivot: result[result.length - 1]?.timestamp || 0,
+      snapshot
+    });
 
     result = mergeWithoutDuplicates(result, newResults);
+
+    if (
+      newResults.length === PAGE_SIZE &&
+      newResults[0].timestamp === newResults[newResults.length - 1].timestamp
+    ) {
+      skip += PAGE_SIZE;
+    } else {
+      skip = 0;
+    }
 
     if (newResults.length < PAGE_SIZE) break;
   }
@@ -66,7 +59,7 @@ export default async function getDelegatesBySpace(
 export function mergeWithoutDuplicates(a: Delegation[], b: Delegation[]) {
   const pivot = a[a.length - 1];
   const pivotDelegations = pivot
-    ? getDelegationFromTimestamp(a, pivot.timestamp)
+    ? getDelegationsFromTimestamp(a, pivot.timestamp)
     : [];
 
   return a.concat(
@@ -83,6 +76,47 @@ function isSameDelegation(a: Delegation, b: Delegation): boolean {
   );
 }
 
-function getDelegationFromTimestamp(results: Delegation[], timestamp: number) {
+function getDelegationsFromTimestamp(results: Delegation[], timestamp: number) {
   return results.filter((r) => r.timestamp === timestamp);
+}
+
+async function fetchData({
+  url,
+  spaces,
+  first,
+  skip,
+  pivot,
+  snapshot
+}: {
+  url: string;
+  spaces: string[];
+  first: number;
+  skip: number;
+  pivot: number;
+  snapshot: string | number;
+}): Promise<Delegation[]> {
+  const params: any = {
+    delegations: {
+      __args: {
+        where: {
+          space_in: spaces,
+          timestamp_gte: pivot
+        },
+        first,
+        skip,
+        orderBy: 'timestamp',
+        orderDirection: 'asc'
+      },
+      delegator: true,
+      space: true,
+      delegate: true,
+      timestamp: true
+    }
+  };
+
+  if (snapshot !== 'latest') {
+    params.delegations.__args.block = { number: snapshot };
+  }
+
+  return (await subgraphRequest(url, params)).delegations || [];
 }
