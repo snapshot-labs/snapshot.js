4d568e1db6fa5422bede1b3027d9a2725a34e23e
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-02-01 00:17:04 +0700

    chore: add more test

--- test/e2e/utils/delegation.spec.ts
+++ test/e2e/utils/delegation.spec.ts
@@ -7,39 +7,48 @@ const NETWORK = '42161';
 const BLOCK_NUMBER = 73900000;
 
 describe('test delegation', () => {
-  test('getDelegatesBySpace should return a list of delegations at specified block', async () => {
-    expect.assertions(2);
-    const results: any = await getDelegatesBySpace(
-      NETWORK,
-      SPACE,
-      BLOCK_NUMBER
-    );
+  describe('getDelegatesBySpace()', () => {
+    test('should return a list of delegations at specified block', async () => {
+      expect.assertions(2);
+      const results: any = await getDelegatesBySpace(
+        NETWORK,
+        SPACE,
+        BLOCK_NUMBER
+      );
 
-    expect(results.length).toEqual(fixtures.length);
-    expect(
-      results.every((a: any) => {
-        return fixtures.some((b) => {
-          return (
-            a.delegator === b.delegator &&
-            a.space === b.space &&
-            a.delegate === b.delegate
-          );
-        });
-      })
-    ).toEqual(true);
-  });
+      expect(results.length).toEqual(fixtures.length);
+      expect(
+        results.every((a: any) => {
+          return fixtures.some((b) => {
+            return (
+              a.delegator === b.delegator &&
+              a.space === b.space &&
+              a.delegate === b.delegate
+            );
+          });
+        })
+      ).toEqual(true);
+    });
 
-  test('getDelegatesBySpace should return an empty array when no results', async () => {
-    expect.assertions(1);
-    const results: any = await getDelegatesBySpace(NETWORK, SPACE, 22531439);
+    test('should return an empty array when no results', async () => {
+      expect.assertions(1);
+      const results: any = await getDelegatesBySpace(NETWORK, SPACE, 22531439);
 
-    expect(results.length).toEqual(0);
-  });
+      expect(results.length).toEqual(0);
+    });
 
-  test('getDelegatesBySpace should reject with a message on unsupported network', async () => {
-    expect.assertions(1);
-    await expect(
-      getDelegatesBySpace('99999', SPACE, BLOCK_NUMBER)
-    ).to.rejects.toMatch(/not available/);
+    test('should reject with a message on unsupported network', async () => {
+      expect.assertions(1);
+      await expect(
+        getDelegatesBySpace('99999', SPACE, BLOCK_NUMBER)
+      ).to.rejects.toMatch(/not available/);
+    });
+
+    test('should raise error on subgraph graphql errors', async () => {
+      expect.assertions(1);
+      await expect(getDelegatesBySpace('1', SPACE, 0)).to.rejects.toMatch(
+        /startBlock/
+      );
+    });
   });
 });
