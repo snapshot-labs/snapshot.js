878ba4e43a9271e389a54e1ddf29023955b438d7
ChaituVR <yourchaitu@gmail.com>
2024-03-25 11:02:00 +0530

    refactor

--- src/sign/index.ts
+++ src/sign/index.ts
@@ -108,15 +108,10 @@ export default class Client {
         .then((res) => {
           if (res.ok) return resolve(res.json());
           if (res.headers.get('content-type')?.includes('application/json'))
-            return res
-              .json()
-              .then((e) => reject(e))
-              .catch((e) => {
-                reject(e);
-              });
+            return res.json().then(reject).catch(reject);
           throw res;
         })
-        .catch((e) => reject(e));
+        .catch(reject);
     });
   }
 
