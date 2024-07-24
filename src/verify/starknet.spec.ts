a4bb52dfd744af623fad94e26ca6f5828af60912
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-07-25 00:21:31 +0900

    chore: add tests

--- src/verify/starknet.spec.ts
+++ src/verify/starknet.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect, describe } from 'vitest';
 import starknetMessage from '../../test/fixtures/starknet/message-alias.json';
+import starknetMessageAlt from '../../test/fixtures/starknet/message-alias-alt.json';
 import verify, { getHash } from './starknet';
 import { validateAndParseAddress } from 'starknet';
 
@@ -21,47 +22,42 @@ describe('verify/starknet', () => {
   });
 
   describe('verify()', () => {
-    test('should return true if the signature is valid', () => {
-      expect(
-        verify(
-          starknetMessage.address,
-          starknetMessage.sig,
-          starknetMessage.data,
-          'SN_SEPOLIA'
-        )
-      ).resolves.toBe(true);
-    });
+    describe.each([
+      ['2', starknetMessage],
+      ['3', starknetMessageAlt]
+    ])('with a %s items signature', (title, message) => {
+      test('should return true if the signature is valid', () => {
+        expect(
+          verify(message.address, message.sig, message.data, 'SN_SEPOLIA')
+        ).resolves.toBe(true);
+      });
 
-    test('should return true if the signature is valid with a padded address', () => {
-      expect(
-        verify(
-          validateAndParseAddress(starknetMessage.address),
-          starknetMessage.sig,
-          starknetMessage.data,
-          'SN_SEPOLIA'
-        )
-      ).resolves.toBe(true);
-    });
+      test('should return true if the signature is valid with a padded address', () => {
+        expect(
+          verify(
+            validateAndParseAddress(message.address),
+            message.sig,
+            message.data,
+            'SN_SEPOLIA'
+          )
+        ).resolves.toBe(true);
+      });
 
-    test('should return true when verifying on a different network', () => {
-      expect(
-        verify(
-          starknetMessage.address,
-          starknetMessage.sig,
-          starknetMessage.data,
-          'SN_MAIN'
-        )
-      ).resolves.toBe(true);
-    });
+      test('should return true when verifying on a different network', () => {
+        expect(
+          verify(message.address, message.sig, message.data, 'SN_MAIN')
+        ).resolves.toBe(true);
+      });
 
-    test('should throw an error if the signature is invalid', () => {
-      expect(
-        verify(
-          '0x7667469b8e93faa642573078b6bf8c790d3a6184b2a1bb39c5c923a732862e1',
-          starknetMessage.sig,
-          starknetMessage.data
-        )
-      ).rejects.toThrow();
+      test('should throw an error if the signature is invalid', () => {
+        expect(
+          verify(
+            '0x7667469b8e93faa642573078b6bf8c790d3a6184b2a1bb39c5c923a732862e1',
+            message.sig,
+            message.data
+          )
+        ).rejects.toThrow();
+      });
     });
   });
 });
