42ee5a9ec8055aa2ed446fea99d7ce89cbfa1622
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-06-05 23:25:33 +0400

    fix: implement own multicall starknet feature, since x-multicall does not support specific block search

--- src/multicall/starknet.ts
+++ src/multicall/starknet.ts
@@ -1,6 +1,12 @@
-import { ContractBatchProvider } from '@argent/x-multicall';
-import { RpcProvider, shortString, uint256 } from 'starknet';
+import { num, RpcProvider, shortString, transaction, uint256 } from 'starknet';
 
+/**
+ * Parses the raw result from a Starknet function call based on its ABI.
+ * It handles different types like felt252, u8, u256, etc., and decodes them accordingly.
+ * @param rawResult - The raw result from the Starknet function call.
+ * @param functionAbi - The ABI of the function that was called.
+ * @returns The parsed result in a more usable format.
+ */
 function parseStarknetResult(rawResult: string[], functionAbi: any): any {
   if (
     !functionAbi ||
@@ -52,45 +58,73 @@ function parseStarknetResult(rawResult: string[], functionAbi: any): any {
   }
 }
 
+/**
+ * Partitions the responses from a Starknet multicall into individual call results.j
+ * Each response starts with its length, followed by the actual response data.
+ * @param responses - The array of responses from the Starknet multicall.
+ * @returns An array of arrays, where each inner array contains the response data for a single call.
+ */
+const partitionResponses = (responses: string[]): string[][] => {
+  if (responses.length === 0) {
+    return [];
+  }
+
+  const [responseLength, ...restResponses] = responses;
+  const responseLengthInt = Number(num.toBigInt(responseLength));
+  const response = restResponses.slice(0, responseLengthInt);
+  const remainingResponses = restResponses.slice(responseLengthInt);
+
+  return [response, ...partitionResponses(remainingResponses)];
+};
+
 export default async function multicall(
   address: string,
   provider: RpcProvider,
   abi: any[],
   calls: any[],
-  limit: number
+  limit: number,
+  options: Record<string, any> = {}
 ) {
-  const multicallProvider = new ContractBatchProvider(provider, address);
+  const callData = calls.map((call) => {
+    return {
+      contractAddress: call[0],
+      entrypoint: call[1],
+      calldata: call[2] || []
+    };
+  });
 
   // Chunk calls into batches based on limit
   const chunks: any[][] = [];
-  for (let i = 0; i < calls.length; i += limit) {
-    chunks.push(calls.slice(i, i + limit));
+  for (let i = 0; i < callData.length; i += limit) {
+    chunks.push(callData.slice(i, i + limit));
   }
 
   // Process each chunk
-  const results = await Promise.all(
+  const paginatedResults = await Promise.all(
     chunks.map((chunk) =>
-      Promise.all(
-        chunk.map(async (callData) => {
-          const [contractAddress, functionName, args] = callData;
-
-          // Make the call using ContractBatchProvider
-          return multicallProvider.callContract({
-            contractAddress,
-            entrypoint: functionName,
-            calldata: args || []
-          });
-        })
+      provider.callContract(
+        {
+          contractAddress: address,
+          entrypoint: 'aggregate',
+          calldata: transaction.fromCallsToExecuteCalldata(chunk)
+        },
+        options.blockTag ?? 'latest'
       )
     )
   );
 
-  // Flatten results from all chunks
-  const flatResults = results.flat();
+  const callResults = paginatedResults
+    .map((callContractResult) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [_blockNumber, _totalLength, ...results] = callContractResult;
+      return partitionResponses(results);
+    })
+    .flat();
 
-  return flatResults.map((rawResult, index) => {
+  return callResults.map((result, index) => {
     const [, functionName] = calls[index];
     const functionAbi = abi.find((item) => item.name === functionName);
-    return [parseStarknetResult(rawResult, functionAbi)];
+
+    return [parseStarknetResult(result, functionAbi)];
   });
 }
