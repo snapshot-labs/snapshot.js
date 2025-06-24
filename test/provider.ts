e967a0508d3e15027941ce6f838112e5ffd70cfb
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-06-24 19:57:59 +0400

    chore: add instruction about test file

--- test/provider.ts
+++ test/provider.ts
@@ -1,8 +1,24 @@
+// This file runs on the compiled files
+// Run `yarn build` to compile the TypeScript files before running this file
+// Run `yarn ts-node test/provider.ts` to run this file
+
 const snapshot = require('../');
 
-const provider = snapshot.utils.getProvider('1');
-const requests = [provider.getNetwork(), provider.getBlockNumber()];
+const evmProvider = snapshot.utils.getProvider('1');
+const evmRequests = [evmProvider.getNetwork(), evmProvider.getBlockNumber()];
 
-Promise.all(requests).then((results) => {
+Promise.all(evmRequests).then((results) => {
+  console.log('EVM Provider Response:');
+  console.log(results);
+});
+
+const starknetProvider = snapshot.utils.getProvider('0x534e5f4d41494e');
+const starknetRequests = [
+  starknetProvider.getChainId(),
+  starknetProvider.getBlockNumber()
+];
+
+Promise.all(starknetRequests).then((results) => {
+  console.log('Starknet Provider Response:');
   console.log(results);
 });
