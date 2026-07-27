3485bdd981bf40f20e05e7f282d5aa99bfda994a
Yash <contact@yashgoyal.dev>
2026-07-27 13:27:48 +0100

    refactor: simplify test structure in blockfinder.spec.ts
    - Reformatted the `getSnapshots` tests for improved readability by removing unnecessary line breaks.
    - Maintained the same functionality and timeout settings while enhancing the overall clarity of the test cases.

--- test/e2e/utils/blockfinder.spec.ts
+++ test/e2e/utils/blockfinder.spec.ts
@@ -4,29 +4,21 @@ import getProvider from '../../../src/utils/provider';
 
 describe('Test block finder', () => {
   const provider = getProvider('1');
-  test(
-    'getSnapshots should work without errors and return object',
-    async () => {
-      expect(
-        await getSnapshots('1', 17789783, provider, ['11155111', '137'])
-      ).toMatchObject({
-        '1': 17789783,
-        '137': 45609596,
-        '11155111': 3979201
-      });
-    },
-    30000
-  );
-  test(
-    'getSnapshots should return all latest if snapshot is latest',
-    async () => {
-      expect(
-        await getSnapshots('1', 'latest', provider, ['11155111', '137'])
-      ).toMatchObject({
-        '137': 'latest',
-        '11155111': 'latest'
-      });
-    },
-    30000
-  );
+  test('getSnapshots should work without errors and return object', async () => {
+    expect(
+      await getSnapshots('1', 17789783, provider, ['11155111', '137'])
+    ).toMatchObject({
+      '1': 17789783,
+      '137': 45609596,
+      '11155111': 3979201
+    });
+  }, 30000);
+  test('getSnapshots should return all latest if snapshot is latest', async () => {
+    expect(
+      await getSnapshots('1', 'latest', provider, ['11155111', '137'])
+    ).toMatchObject({
+      '137': 'latest',
+      '11155111': 'latest'
+    });
+  }, 30000);
 });
